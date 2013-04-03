362191e21e904d53bc828da94f4fd70561c2a8ee
Tom Boutell <tom@punkave.com>
2013-04-03 11:44:26 -0400

    Moved more code to content.js in anticipation of not loading editor.js eventually for most people:
    * Modals
    * afterYield
    * fromTemplate
    * getRadio/setRadio
    
    Added apos.outerHTML; RTE uses it

--- public/js/editor.js
+++ public/js/editor.js
@@ -1470,13 +1470,9 @@ apos.parseArea = function(content) {
         _.extend(item, data);
         items.push(item);
       } else {
-        // This is a rich text element like <strong> or <h3>
-        //
-        // @#)(*$ absence of outerHTML in some browsers, work around that
-        // with a lazy but simple jQuery hack
-        var wrapper = $('<div></div>');
-        wrapper.append($(child).clone());
-        richText += wrapper.html();
+        // This is a rich text element like <strong> or <h3>.
+        // We need the markup for the entire thing
+        richText += apos.outerHTML(child);
       }
     }
   }
@@ -1485,200 +1481,6 @@ apos.parseArea = function(content) {
   flushRichText();
 
   return items;
-};
-
-apos._modalStack = [];
-
-apos._modalInitialized = false;
-
-// We have a stack of modals going, we want to add a blackout to the
-// previous one, or the body if this is the first modal
-apos.getTopModalOrBody = function() {
-  return $(apos._modalStack.length ? apos._modalStack[apos._modalStack.length - 1] : 'body');
-};
-
-// Be sure to read about apos.modalFromTemplate too, as that is usually
-// the easiest way to present a modal.
-
-// apos.modal displays the element specified by sel as a modal dialog. Goes 
-// away when the user clicks .apos-save or .apos-cancel, or submits the form
-// element in the modal (implicitly saving), or presses escape.
-
-// options.init can be an async function to populate the
-// modal with content (usually used with apos.modalFromTemplate, below).
-// If you pass an error as the first argument to the callback the
-// modal will not appear and options.afterHide will be triggered immediately.
-// Don't forget to call the callback. 
-//
-// Note that apos.modal is guaranteed to return *before* options.init is called, 
-// so you can refer to $el in a closure. This is useful if you are using
-// apos.modalFromTemplate to create $el.
-
-// options.afterHide can be an asynchronous function to do something
-// after the modal is dismissed (for any reason, whether saved or cancelled), 
-// like removing it from the DOM if that is appropriate.
-// Don't forget to call the callback. Currently passing an error
-// to the afterHide callback has no effect.
-
-// options.save can be an asynchronous function to do something after
-// .apos-save is clicked (or enter is pressed in the only text field).
-// It is invoked before afterHide. If you pass an error to the callback,
-// the modal is NOT dismissed, allowing you to do validation. If it does
-// not exist then the save button has no hardwired function (and need not be present).
-
-// If you wish to dismiss the dialog for any other reason, just trigger
-// an event on the modal dialog element:
-
-// $el.trigger('aposModalHide')
-
-// Focus is automatically given to the first visible form element
-// that does not have the apos-filter class.
-
-// Modals may be nested and the right thing will happen.
-
-apos.modal = function(sel, options) {
-
-  if (!apos._modalInitialized) {
-    apos._modalInitialized = true;
-    // Just ONE event handler for the escape key so we don't have
-    // modals falling all over themselves to hide each other
-    // consecutively.
-
-    // Escape key should dismiss the top modal, if any
-
-    $(document).on('keyup.aposModal', function(e) {
-      if (e.keyCode === 27) {
-        var topModal = apos.getTopModalOrBody();
-        if (topModal.filter('.apos-modal')) {
-          topModal.trigger('aposModalHide');
-          return false;
-        } else {
-          return true;
-        }
-      }
-      return true;
-    });
-  }
-
-  var $el = $(sel);
-
-  if (!options) {
-    options = {};
-  }
-
-  _.defaults(options, {
-    init: function(callback) {callback(null);},
-    save: function(callback) {callback(null);},
-    afterHide: function(callback) {callback(null);}
-  });
-
-  $el.on('aposModalHide', function() {
-    // TODO consider what to do if this modal is not the top.
-    // It's tricky because some need to be removed rather than
-    // merely hid. However we don't currently dismiss modals other
-    // than the top, so...
-    apos._modalStack.pop();
-    var blackoutContext = apos.getTopModalOrBody();
-    blackoutContext.find('.apos-modal-blackout').remove();
-    $el.hide();
-    apos.popSelection();
-    options.afterHide(function(err) {
-      return;
-    });
-  });
-
-  function hideModal() {
-    $el.trigger('aposModalHide');
-    return false;
-  }
-
-  // Enter key driven submits of the form should act like a click on the save button,
-  // do not try to submit the form old-school
-  $el.on('submit', 'form', function() {
-    $el.find('.apos-save').click();
-    return false;
-  });
-
-  $el.on('click', '.apos-cancel', hideModal);
-
-  $el.on('click', '.apos-save', function() {
-    options.save(function(err) {
-      if(!err) {
-        hideModal();
-      }
-    });
-    return false;
-  });
-
-  apos.afterYield(function() {
-    options.init(function(err) {
-      if (err) {
-        hideModal();
-        return;
-      }
-      apos.pushSelection();
-      var blackoutContext = apos.getTopModalOrBody();
-      var blackout = $('<div class="apos-modal-blackout"></div>');
-      blackoutContext.append(blackout);
-      apos._modalStack.push($el);
-      $('body').append($el);
-      $el.offset({ top: $('body').scrollTop() + 100, left: ($(window).width() - $el.outerWidth()) / 2 });
-      $el.show();
-      // Give the focus to the first form element. (Would be nice to
-      // respect tabindex if it's present, but it's rare that
-      // anybody bothers)
-      $el.find("form:not(.apos-filter) :input:visible:enabled:first").focus();
-    });
-  });
-
-  return $el;
-};
-
-// Clone the element matching the specified selector that
-// also has the apos-template class, remove the apos-template
-// class from the clone, and present it as a modal. This is a
-// highly convenient way to present modals based on templates
-// present in the DOM (note that the .apos-template class hides
-// things until they are cloned). Accepts the same options as
-// apos.modal, above. Returns a jquery object referring
-// to the modal dialog element. Note that this method always
-// returns *before* the init method is invoked.
-
-apos.modalFromTemplate = function(sel, options) {
-
-  var $el = apos.fromTemplate(sel);
-
-  // Make sure they can provide their own afterHide
-  // option, and that we don't remove $el until
-  // after it invokes its callback
-
-  var afterAfterHide = options.afterHide;
-  if (!afterAfterHide) {
-    afterAfterHide = function(callback) {
-      return callback(null);
-    };
-  }
-  options.afterHide = function(callback) {
-    afterAfterHide(function(err) {
-      $el.remove();
-      return callback(err);
-    });
-  };
-
-  return apos.modal($el, options);
-};
-
-// Do something after control returns to the browser (after you return from
-// whatever event handler you're in). In old browsers the setTimeout(fn, 0)
-// technique is used. In the latest browsers setImmediate is used, because
-// it's faster (although it has a confusing name)
-
-apos.afterYield = function(fn) {
-  if (window.setImmediate) {
-    return window.setImmediate(fn);
-  } else {
-    return setTimeout(fn, 0);
-  }
 };
 
 // We use this to save the selection before starting
@@ -1778,17 +1580,6 @@ apos.cleanWidgetData = function(data) {
   return data;
 };
 
-// Clone the element matching the selector which also has the
-// .apos-template class, remove that class from the clone, and
-// return the clone. This is convenient for turning invisible templates in
-// the DOM into elements ready to add to the DOM.
-
-apos.fromTemplate = function(sel) {
-  var $item = $(sel).filter('.apos-template').clone();
-  $item.removeClass('apos-template');
-  return $item;
-};
-
 // We often submit the content of an area as part of a regular POST. This is
 // a good way to pack it up
 apos.stringifyArea = function($editable) {
@@ -1861,22 +1652,5 @@ apos.tagsToString = function(s) {
   }
   var result = s.join(', ');
   return result;
-};
-
-// RADIO BUTTON CONVENIENCE FUNCTIONS
-
-// Anywhere you have a form and want to manipulate it with jQuery,
-// these will get you past the nonsense of val() not working
-// because there is more than one element involved. Just select
-// all the radio buttons by name and pass to these.
-
-apos.setRadio = function($els, value) {
-  $.each($els, function() {
-    $(this).attr('checked', $(this).attr('value') === value);
-  });
-};
-
-apos.getRadio = function($els) {
-  return $els.filter(':checked').val();
 };
 
