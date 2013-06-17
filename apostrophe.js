2bb3eb3a03186fee4593acd9a6ec9814ae43c03e
Tom Boutell <tom@punkave.com>
2013-06-17 15:06:29 -0400

    Publish the home page and the search page in apostrophe:reset

--- apostrophe.js
+++ apostrophe.js
@@ -4363,7 +4363,7 @@ function Apos() {
       }
       return async.series([ resetMain ], callback);
       function resetMain(callback) {
-        return self.pages.insert([{ slug: '/', _id: '4444444444444', path: 'home', title: 'Home', level: 0, type: 'home' }, { slug: '/search', _id: 'search', orphan: true, path: 'home/search', title: 'Search', level: 1, type: 'search', rank: 9998 }, { slug: '/trash', _id: 'trash', path: 'home/trash', title: 'Trash', level: 1, trash: true, type: 'trash', rank: 9999 }], callback);
+        return self.pages.insert([{ slug: '/', _id: '4444444444444', path: 'home', title: 'Home', level: 0, type: 'home', published: true }, { slug: '/search', _id: 'search', orphan: true, path: 'home/search', title: 'Search', level: 1, type: 'search', rank: 9998, published: true }, { slug: '/trash', _id: 'trash', path: 'home/trash', title: 'Trash', level: 1, trash: true, type: 'trash', rank: 9999 }], callback);
       }
     });
   };
