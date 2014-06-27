00c3d739a368ea32717efdfdaf55d06685bb3b61
Fotis Paraskevopoulos <fotis@existanze.com>
2014-06-27 11:33:38 +0300

    Adding the ability to override the modal top offset by adding a data-top-offset attribute using a dataAttrs block

--- public/js/content.js
+++ public/js/content.js
@@ -448,10 +448,13 @@ apos.modal = function(sel, options) {
       $el.data('aposSavedScrollTop', $(window).scrollTop());
       apos._modalStack.push($el);
       $('body').append($el);
+      var dataTopOffset = $el.attr("data-top-offset");
       var offset;
       if ($el.hasClass('apos-modal-full-page')) {
         offset = $('.apos-admin-bar').height();
-      } else {
+      } else if(dataTopOffset !== undefined ) {
+        offset = parseInt(dataTopOffset);
+      }else{
         offset = 100;
       }
       $el.offset({ top: $('body').scrollTop() + offset, left: ($(window).width() - $el.outerWidth()) / 2 });
