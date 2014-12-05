8ae9914d62582d022a9436a2afb635e9824ce009
Samantha <suhmantha1@gmail.com>
2014-12-05 11:09:28 -0500

    Added group option to files widget; Fixed files style so titles can be read; Removed gear as there are no useful extra fields

--- public/js/widgets/editors/files.js
+++ public/js/widgets/editors/files.js
@@ -7,14 +7,7 @@ function AposFilesWidgetEditor(options)
   options.showImages = false;
   options.template = '.apos-files-editor';
   options.type = 'files';
-  // We want the default for extra fields to be true rather than false for
-  // this widget
-  if (!options.options) {
-    options.options = {};
-  }
-  if (options.options.extraFields === undefined) {
-    options.options.extraFields = true;
-  }
+
   // Explicitly avoid limiting to a particular type of file
   self.fileGroup = null;
   AposSlideshowWidgetEditor.call(self, options);
