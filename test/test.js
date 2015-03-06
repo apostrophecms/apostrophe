325a3447bb16111389c4cf6bbbdd763ded628e07
Jimmy Harrison <jtharrison91@gmail.com>
2015-03-06 12:54:23 -0500

    remove file reset stuff from root test file

--- test/test.js
+++ test/test.js
@@ -15,27 +15,6 @@ require('./bootstraping.js');
 
 describe('Modules', function(){
 
-  // Clear out uploads after tests wrap up
-  after(function() {
-    deleteFolderRecursive(__dirname + '/public/uploads');
-
-    function deleteFolderRecursive (path) {
-      var files = [];
-      if( fs.existsSync(path) ) {
-        files = fs.readdirSync(path);
-        files.forEach(function(file,index){
-          var curPath = path + "/" + file;
-          if(fs.lstatSync(curPath).isDirectory()) { // recurse
-            deleteFolderRecursive(curPath);
-          } else { // delete file
-            fs.unlinkSync(curPath);
-          }
-        });
-        fs.rmdirSync(path);
-      }
-    };
-  });
-
   //                   //
   //    BASE MODULE    //
   //                   //
