f54d701b5223c3d9f4cf4f7458772d2978806c73
Tom Boutell <tom@punkave.com>
2015-04-01 16:28:16 -0400

    tests passing except the as yet unwritten emptyTrash

--- test/docs.js
+++ test/docs.js
@@ -397,6 +397,7 @@ describe('Docs', function() {
 
   it('should have a "rescue" method on docs that removes the "trash" property from an object', function(done) {
     apos.docs.rescue(adminReq(), { slug: 'carl' }, function(err, object) {
+      console.error(err);
       assert(!err);
       assert(object);
       // the object should no longer have a trash property
