db059dea5dea0103a674e913825065ab75fe2d16
Tom Boutell <tom@punkave.com>
2015-08-01 17:26:45 -0400

    Close #322 Close #323

--- test/versions.js
+++ test/versions.js
@@ -26,11 +26,11 @@ function adminReq() {
 }
 
 
-describe('Docs', function() {
+describe('Versions', function() {
 	//////
   // EXISTENCE
   //////
-  it('should have a collection to insert docs into', function(done) {
+  it('should should be a module', function(done) {
     apos = require('../index.js')({
       root: module,
       shortName: 'test',
@@ -59,7 +59,8 @@ describe('Docs', function() {
   //////
   // Versioning
   //////
-  it('should be able to insert a new doc', function(done) {
+
+  it('inserting a doc should result in a version', function(done) {
     var object = {
       slug: 'one',
       published: true,
@@ -70,26 +71,25 @@ describe('Docs', function() {
       alive: true
     };
 
-    var docId = '';
     apos.docs.insert(adminReq(), object, function(err, object) {
       assert(!err);
       assert(object);
       assert(object._id);
-      docId = object._id;
-      done();
-    });
-    //did the versions module kick-in?
-    apos.versions.db.find({ docId: docId }).toArray(function(err,docs){
-    	assert(!err);
-    	// we should have a document
-      assert(docs);
-      // there should be only one document in our results
-      assert(docs.length === 1);
-      //does it have a property match?
-      assert(docs[0].age === 15);
-      done();
+      var docId = object._id;
+      //did the versions module kick-in?
+      apos.versions.db.find({ docId: docId }).toArray(function(err, versions) {
+        assert(!err);
+        // we should have a document
+        assert(versions);
+        // there should be only one document in our results
+        assert(versions.length === 1);
+        //does it have a property match?
+        assert(versions[0].doc.age === 15);
+        done();
+      });
     });
   });
+
   it('should be able to update', function(done) {
     var cursor = apos.docs.find(adminReq(), { slug: 'one' }).toArray(function(err,docs){
       assert(!err);
@@ -110,49 +110,62 @@ describe('Docs', function() {
         assert(object.alive === false);
 
         //did the versions module kick-in?
-	      apos.versions.db.find({ docId: object._id }).sort({createdAt: -1}).toArray(function(err,docs){
+	      apos.versions.db.find({ docId: object._id }).sort({createdAt: -1}).toArray(function(err,versions){
 		    	assert(!err);
 		      // we should have a document
-		      assert(docs);
+		      assert(versions);
 		      // there should be two documents now in our results
-		      assert(docs.length === 2);
+		      assert(versions.length === 2);
 		      // the property should have been updated
-		      assert(docs[0].alive === false);
-		      assert(docs[1].alive === true);
+		      assert(versions[0].doc.alive === false);
+		      assert(versions[1].doc.alive === true);
 		      done();
 		    });
       });
     });
   });
-  it('should be able to revert to a previous verions', function(done){
-  	//TODO write this thing
-  	assert(false);
-  	done();
-  });
 
-
-  //////
-  // Disable
-  //////
-  it('should not version pages if not set to enabled', function(done) {
-    apos = require('../index.js')({
-      root: module,
-      shortName: 'test',
-      hostName: 'test.com',
-      modules: {
-        'apostrophe-express': {
-          secret: 'xxx',
-          port: 7949
-        },
-        'apostrophe-doc-versions':{
-        	enabled: false
-        }
-      },
-      afterInit: function(callback) {
-        apos.argv._ = [];
-        assert(!apos.versions.db);
-        return callback(null);
-      }
+  it('should be able to revert to a previous version', function(done){
+    apos.docs.find(adminReq(), { slug: 'one' }).toObject(function(err,doc) {
+      apos.versions.db.find({ docId: doc._id }).sort({createdAt: -1}).toArray(function(err, versions) {
+        assert(versions.length === 2);
+        apos.versions.revert(adminReq(), doc, versions[1], function(err) {
+          assert(!err);
+          // make sure the change propagated to the database
+          apos.docs.find(adminReq(), { slug: 'one' }).toObject(function(err,doc) {
+            assert(!err);
+            assert(doc);
+            assert(doc.alive === true);
+            done();
+          });
+        });
+      });
     });
   });
+
+  //////
+  // When disabled the module does not create versions,
+  // and docs can still be inserted
+  //////
+  // it('should not version pages if not set to enabled', function(done) {
+  //   apos = require('../index.js')({
+  //     root: module,
+  //     shortName: 'test',
+  //     hostName: 'test.com',
+  //     modules: {
+  //       'apostrophe-express': {
+  //         secret: 'xxx',
+  //         port: 7949
+  //       },
+  //       'apostrophe-doc-versions':{
+  //       	enabled: false
+  //       }
+  //     },
+  //     afterInit: function(callback) {
+  //       apos.argv._ = [];
+  //       assert(!apos.versions.db);
+  //       return callback(null);
+  //     }
+  //   });
+  // });
 });
