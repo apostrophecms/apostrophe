ae7d2143dbeedd8923ec7cb5ff95eb1c9f81c4dc
Tom Boutell <tom@punkave.com>
2016-03-01 12:41:38 -0500

    prepublish script typo broke it for a sec there

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 VERSION=`cat package.json | grep \"version\": | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+'`
-perl -pi -e "s/apos.version = \"[0-9\.]+\"/apos.version = \"$VERSION\"/"public/js/content.js &&
+perl -pi -e "s/apos.version = \"[0-9\.]+\"/apos.version = \"$VERSION\"/" public/js/content.js &&
 git add -A public/js/content.js &&
 git commit -m "bump version number in client side javascript" &&
 git push
