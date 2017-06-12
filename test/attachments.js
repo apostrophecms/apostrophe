edec280764ca0f0a94812812f1205978539ccf13
Tom Boutell <tom@punkave.com>
2017-06-12 12:01:27 -0400

    2.26.0:
    * Do not crash on missing widget types, print good warning messages.
    
    * Complete implementation of the [explicitOrder](http://apostrophecms.org/docs/modules/apostrophe-docs/server-apostrophe-cursor.html#explicit-order) cursor filter, replacing a nonfunctional implementation.
    
    * If the mongodb connection is lost, the default behavior is now to retry it forever, so when MongoDB does get restarted Apostrophe will find it. In addition, a `connect` object may be passed to the `apostrophe-db` module to be passed on to the MongoDB connect call.
    
    * Spaces added between DOM attributes for better HTML5 compliance.
    
    * `required` subfields are now enforced when editing fields of type `array`.
    
    Thanks to Michelin for their support of much of the work in this release.

--- test/attachments.js
+++ test/attachments.js
@@ -13,8 +13,7 @@ describe('Attachment', function() {
   var uploadTarget = __dirname + "/public/uploads/attachments/";
   var collectionName = 'aposAttachments';
 
-  function wipeIt() {
-    apos.db.collection(collectionName).drop();
+  function wipeIt(callback) {
     deleteFolderRecursive(__dirname + '/public/uploads');
 
     function deleteFolderRecursive (path) {
@@ -32,6 +31,13 @@ describe('Attachment', function() {
         fs.rmdirSync(path);
       }
     }
+
+    apos.db.collection(collectionName, function(err, collection) {
+      assert(!err);
+      assert(collection);
+      collection.remove({}, callback);
+    });
+
   }
 
   // after(wipeIt);
@@ -63,14 +69,17 @@ describe('Attachment', function() {
       }
     });
   });
+  
+  describe('wipe', function() {
+    it('should clear previous material if any', function(done) {
+      wipeIt(done);
+    });
+  });
 
   var request = require('request');
   var fs = require('fs');
 
   describe('accept', function() {
-    before(function() {
-      wipeIt();
-    });
 
     function accept(filename, callback) {
       return apos.attachments.accept(t.req.admin(apos), {
