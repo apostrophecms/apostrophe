52b1515372d9ef50c595d1cf987fe99fc4a0096e
Tom Boutell <tom@punkave.com>
2017-07-07 16:18:04 -0400

    admin bar tests account for new pages button

--- test/admin-bar.js
+++ test/admin-bar.js
@@ -44,10 +44,10 @@ describe('Admin bar', function() {
       afterInit: function(callback) {
         assert(apos.modules['apostrophe-admin-bar']);
         assert(apos.adminBar);
-        assert(apos.adminBar.items.length === 7);
-        assert(apos.adminBar.items[4].name === 'apostrophe-login-logout');
-        assert(apos.adminBar.items[5].name === 'apostrophe-files');
-        assert(apos.adminBar.items[6].name === 'apostrophe-images');
+        assert(apos.adminBar.items.length === 8);
+        assert(apos.adminBar.items[5].name === 'apostrophe-login-logout');
+        assert(apos.adminBar.items[6].name === 'apostrophe-files');
+        assert(apos.adminBar.items[7].name === 'apostrophe-images');
         // In tests this will be the name of the test file,
         // so override that in order to get apostrophe to
         // listen normally and not try to run a task. -Tom
@@ -95,10 +95,10 @@ describe('Admin bar', function() {
       afterInit: function(callback) {
         assert(apos.modules['apostrophe-admin-bar']);
         assert(apos.adminBar);
-        assert(apos.adminBar.items.length === 7);
-        assert(apos.adminBar.items[4].name === 'apostrophe-files');
-        assert(apos.adminBar.items[5].name === 'apostrophe-images');
-        assert(apos.adminBar.items[6].name === 'apostrophe-login-logout');
+        assert(apos.adminBar.items.length === 8);
+        assert(apos.adminBar.items[5].name === 'apostrophe-files');
+        assert(apos.adminBar.items[6].name === 'apostrophe-images');
+        assert(apos.adminBar.items[7].name === 'apostrophe-login-logout');
         // In tests this will be the name of the test file,
         // so override that in order to get apostrophe to
         // listen normally and not try to run a task. -Tom
