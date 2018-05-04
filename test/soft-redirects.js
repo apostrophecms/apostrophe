9e709fc58a9cb8bee376241a4b8fdc4e2742c451
Leonhard Melzer <leonhard.melzer@gmail.com>
2018-05-04 13:19:13 +0200

    Implement HTTP status code as option passed to module as per PR feedback.

--- test/soft-redirects.js
+++ test/soft-redirects.js
@@ -75,17 +75,90 @@ describe('Soft Redirects', function() {
   it('should be able to serve the page at its old URL too, via redirect', function(done) {
     return request({
       url: 'http://localhost:7900/child',
-      followRedirect: response => {
-        assert.equal(response.statusCode, 301);
-
-        return true;
-      }
+      followRedirect: false
     }, function(err, response, body) {
+      assert(!err);
+      // Is our status code good?
+      assert.equal(response.statusCode, 302);
+      // Are we going to be redirected to our page?
+      assert.equal(response.headers['location'], '/child-moved');
+      return done();
+    });
+  });
+
+});
+
+describe('Soft Redirects - with `redirectHttpStatusCode` option', function() {
+
+  this.timeout(t.timeout);
+
+  after(function(done) {
+    return t.destroy(apos, done);
+  });
+
+  it('should exist', function(done) {
+    apos = require('../index.js')({
+      root: module,
+      shortName: 'test',
+
+      modules: {
+        'apostrophe-express': {
+          port: 7900,
+          secret: 'test'
+        },
+        'apostrophe-pages': {
+          park: [
+            {
+              parkedId: 'child',
+              title: 'Child',
+              slug: '/child',
+              type: 'default',
+              published: true
+            }
+          ]
+        },
+        'apostrophe-soft-redirects': {
+          redirectHttpStatusCode: 301
+        }
+      },
+      afterInit: function(callback) {
+        assert(apos.modules['apostrophe-soft-redirects']);
+        assert.equal(apos.modules['apostrophe-soft-redirects'].options.redirectHttpStatusCode, 301);
+        apos.argv._ = [];
+        return callback(null);
+      },
+      afterListen: function(err) {
+        assert(!err);
+        done();
+      }
+    });
+  });
+
+  it('should be able to serve the /child page (which also populates historicUrls)', function(done) {
+    return request('http://localhost:7900/child', function(err, response, body) {
       assert(!err);
       // Is our status code good?
       assert.equal(response.statusCode, 200);
       // Did we get our page back?
       assert(body.match(/Default Page Template/));
+      return done();
+    });
+  });
+
+  it('should be able to change the URL via db', function() {
+    return apos.docs.db.update({ slug: '/child' }, { $set: { slug: '/child-moved' } });
+  });
+
+  it('should be able to serve the page at its old URL too, via redirect', function(done) {
+    return request({
+      url: 'http://localhost:7900/child',
+      followRedirect: false
+    }, function(err, response, body) {
+      assert(!err);
+      // Is our status code good?
+      assert.equal(response.statusCode, 301);
+      // Are we going to be redirected to our page?
+      assert.equal(response.headers['location'], '/child-moved');
       return done();
     });
   });
