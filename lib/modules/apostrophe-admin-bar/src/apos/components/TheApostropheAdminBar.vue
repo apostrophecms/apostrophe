62e352d9c204e043d5b71103755ccfd2d232efee
bgantick <brian@punkave.com>
2018-09-26 11:45:25 -0400

    Admin bar items emit events now

--- lib/modules/apostrophe-admin-bar/src/apos/components/TheApostropheAdminBar.vue
+++ lib/modules/apostrophe-admin-bar/src/apos/components/TheApostropheAdminBar.vue
@@ -6,13 +6,16 @@
           A
         </div>
         <div v-for="item in items" class="apos-admin-bar-item">
-          <div class="apos-admin-bar-item-inner" :data-apos-admin-bar-item="item.name">
-            <component :is="item.options.href ? 'a' : 'span'" :href="item.options.href">
+          <div class="apos-admin-bar-item-inner">
+            <component v-if="item.options" :is="item.options.href ? 'a' : 'button'" :href="item.options.href" :data-apos-admin-bar-item="item.name" v-on="item.options.href ? {} : { click: () => emitEvent(item.name) }">
               {{ item.label }}
             </component>
+            <button v-else :data-apos-dropdown="item.menu">{{ item.label }}</button>
             <ul v-if="item.menu" class="apos-dropdown-items" data-apos-dropdown-items>
               <li v-for="subItem in item.items" class="apos-dropdown-item" :data-apos-admin-bar-item="subItem.name">
-                {{ subItem.label }}
+                <component v-if="subItem.options" :is="subItem.options.href ? 'a' : 'button'" :href="subItem.options.href" :data-apos-admin-bar-item="subItem.name" v-on="subItem.options.href ? {} : { click: () => emitEvent(subItem.name) }">
+                  {{ subItem.label }}
+                </component>
               </li>
             </ul>
           </div>
@@ -27,6 +30,11 @@ export default {
   name: 'TheApostropheAdminBar',
   props: {
     items: Array
+  },
+  methods: {
+    emitEvent: function (name) {
+      this.$emit('adminBarItem', name);
+    }
   }
 }
 </script>
