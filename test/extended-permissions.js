fbc8618f4a88f04bf10b79b3d271a460e750816a
Thomas Boutell <tom@punkave.com>
2019-03-06 11:42:34 -0500

    more tests

--- test/extended-permissions.js
+++ test/extended-permissions.js
@@ -74,6 +74,134 @@ describe('Permissions', function() {
     it('prohibits view-doc for public without published', function() {
       assert(!apos.permissions.can(req(), 'view-doc', {}));
     });
+    it('permits view-doc for guest user with loginRequired', function() {
+      assert(apos.permissions.can(req({ user: { _permissions: { guest: 1 } } }), 'view-doc', { published: true, loginRequired: 'loginRequired' }));
+    });
+    it('permits view-doc for individual with proper id', function() {
+      assert(apos.permissions.can(req({ user: { _id: 1 } }), 'view-doc', { published: true, loginRequired: 'certainUsers', docPermissions: [ 'view-1' ] }));
+    });
+    it('forbids view-doc for individual with wrong id', function() {
+      assert(!apos.permissions.can(req({ user: { _id: 2 } }), 'view-doc', { published: true, loginRequired: 'certainUsers', docPermissions: [ 'view-1' ] }));
+    });
+    it('permits view-doc for individual with group id', function() {
+      assert(apos.permissions.can(req({ user: { _id: 1, groupIds: [ 1001, 1002 ] } }), 'view-doc', { published: true, loginRequired: 'certainUsers', docPermissions: [ 'view-1002' ] }));
+    });
+    it('forbids view-doc for individual with wrong group id', function() {
+      assert(!apos.permissions.can(req({ user: { _id: 2, groupIds: [ 1001, 1002 ] } }), 'view-doc', { published: true, loginRequired: 'certainUsers', docPermissions: [ 'view-1003' ] }));
+    });
+    it('certainUsers will not let you slide past to an unpublished doc', function() {
+      assert(!apos.permissions.can(req({ user: { _id: 1 } }), 'view-doc', { loginRequired: 'certainUsers', docPermissions: [ 'view-1' ] }));
+    });
+    it('permits view-doc for unpublished doc for individual with group id for editing and the update permission for the type', function() {
+      assert(apos.permissions.can(req({ user: { _id: 1, groupIds: [ 1001, 1002 ], _permissions: { 'update-turkey': true } } }), 'view-doc', { type: 'turkey', docPermissions: [ 'edit-1002' ] }));
+    });
+    it('permits view-doc for unpublished doc for individual with the updateany permission for the type', function() {
+      assert(apos.permissions.can(req({ user: { _id: 1, _permissions: { 'updateany-turkey': true } } }), 'view-doc', { type: 'turkey', docPermissions: [ 'edit-1002' ] }));
+    });
+    it('permits insert-doc for individual with the insert permission', function() {
+      assert(apos.permissions.can(req({ user: { _id: 1, _permissions: { 'insert-turkey': true } } }), 'insert-turkey'));
+    });
+    it('forbids insert-doc for individual without the insert permission', function() {
+      assert(!apos.permissions.can(req({ user: { _id: 1, _permissions: {} } }), 'insert-turkey'));
+    });
+    it('permits update-doc for individual with the updateany permission', function() {
+      assert(apos.permissions.can(req({ user: { _id: 1, _permissions: { 'updateany-turkey': true } } }), 'update-doc', { type: 'turkey', docPermissions: [ 'edit-1002' ] }));
+    });
+    it('permits update-doc for individual with group id for edit- and the update-turkey permission', function() {
+      assert(apos.permissions.can(req({ user: { _id: 1, groupIds: [ 1001, 1002 ], _permissions: { 'update-turkey': true } } }), 'update-doc', { type: 'turkey', docPermissions: [ 'edit-1002' ] }));
+    });
+    it('forbids update-doc for individual with group id for editing but no update-turkey permission', function() {
+      assert(!apos.permissions.can(req({ user: { _id: 1, groupIds: [ 1001, 1002 ] } }), 'update-doc', { docPermissions: [ 'edit-1002' ] }));
+    });
+    it('forbids update-doc for other person', function() {
+      assert(!apos.permissions.can(req({ user: { _id: 7 } }), 'update-doc', { docPermissions: [ 'edit-1002' ] }));
+    });
+    it('permits trash-doc for individual with update permission for the doc and the trash permission for turkeys', function() {
+      assert(apos.permissions.can(req({ user: { _id: 1, groupIds: [ 1001, 1002 ], _permissions: { 'update-turkey': true, 'trash-turkey': true } } }), 'trash-doc', { type: 'turkey', docPermissions: [ 'edit-1002' ] }));
+    });
+    it('forbids trash-doc for individual with update permission for the doc but no trash permission for turkeys', function() {
+      assert(!apos.permissions.can(req({ user: { _id: 1, groupIds: [ 1001, 1002 ], _permissions: { 'update-turkey': true } } }), 'trash-doc', { type: 'turkey', docPermissions: [ 'edit-1002' ] }));
+    });
+    it('can insert test turkey objects for criteria tests', function() {
+      return apos.docs.db.insert([
+        {
+          _id: 'published',
+          published: true
+        },
+        {
+          _id: 'unpublished',
+          published: false
+        },
+        {
+          _id: 'loginrequired',
+          published: true,
+          loginRequired: 'loginRequired'
+        },
+        {
+          _id: 'certainpeople',
+          published: true,
+          loginRequired: 'certainPeople',
+          docPermissions: [ 'view-1002' ]
+        },
+        {
+          _id: 'certainpeopleendrun',
+          published: false,
+          loginRequired: 'certainPeople',
+          docPermissions: [ 'view-1002' ]
+        },
+        {
+          _id: 'edit1002',
+          type: 'turkey',
+          docPermissions: [ 'edit-1002' ]
+        }
+      ]);
+    });
+
+    it('permits view-doc for public without loginRequired', function() {
+      return criteriaTest(req(), 'view-doc', 'published', true);
+    });
+    it('prohibits view-doc for public without published', function() {
+      return criteriaTest(req(), 'view-doc', 'unpublished', false);
+    });
+    it('prohibits view-doc for public with loginRequired', function() {
+      return criteriaTest(req(), 'view-doc', 'loginrequired', false);
+    });
+    it('permits view-doc for guest user with loginRequired', function() {
+      return criteriaTest(req({ user: { _permissions: { guest: 1 } } }), 'view-doc', 'loginrequired', true);
+    });
+    it('permits view-doc for individual with proper id', function() {
+      assert(apos.permissions.can(req({ user: { _id: 1 } }), 'view-doc', { published: true, loginRequired: 'certainUsers', docPermissions: [ 'view-1' ] }));
+    });
+    it('forbids view-doc for individual with wrong id', function() {
+      assert(!apos.permissions.can(req({ user: { _id: 2 } }), 'view-doc', { published: true, loginRequired: 'certainUsers', docPermissions: [ 'view-1' ] }));
+    });
+    it('permits view-doc for individual with group id', function() {
+      assert(apos.permissions.can(req({ user: { _id: 1, groupIds: [ 1001, 1002 ] } }), 'view-doc', { published: true, loginRequired: 'certainUsers', docPermissions: [ 'view-1002' ] }));
+    });
+    it('forbids view-doc for individual with wrong group id', function() {
+      assert(!apos.permissions.can(req({ user: { _id: 2, groupIds: [ 1001, 1002 ] } }), 'view-doc', { published: true, loginRequired: 'certainUsers', docPermissions: [ 'view-1003' ] }));
+    });
+    it('certainUsers will not let you slide past to an unpublished doc', function() {
+      assert(!apos.permissions.can(req({ user: { _id: 1 } }), 'view-doc', { loginRequired: 'certainUsers', docPermissions: [ 'view-1' ] }));
+    });
+    it('permits view-doc for unpublished doc for individual with group id for editing and the update permission for the type', function() {
+
+        {
+          _id: 'edit1002',
+          type: 'turkey',
+          docPermissions: [ 'edit-1002' ]
+        },
+
+    });
+    it('forbids view-doc for public with loginRequired', function() {
+      assert(!apos.permissions.can(req(), 'view-doc', { published: true, loginRequired: 'loginRequired' }));
+    });
+    it('permits view-doc for public without loginRequired', function() {
+      assert(apos.permissions.can(req(), 'view-doc', { published: true }));
+    });
+    it('prohibits view-doc for public without published', function() {
+      assert(!apos.permissions.can(req(), 'view-doc', {}));
+    });
     it('prohibits view-doc for public with loginRequired', function() {
       assert(!apos.permissions.can(req(), 'view-doc', { published: true, loginRequired: 'loginRequired' }));
     });
