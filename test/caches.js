652a8a14d12835fd4aaf844e9c11ca606d7e44e7
Thomas Boutell <tom@punkave.com>
2019-04-09 14:56:12 -0400

    caches tests pass

--- test/caches.js
+++ test/caches.js
@@ -3,110 +3,45 @@ let assert = require('assert');
 
 describe('Caches', function() {
 
-  after(function(done) {
-    return t.destroy(apos, done);
+  after(async function() {
+    return t.destroy(apos);
   });
 
   this.timeout(t.timeout);
 
   let apos;
   let cache;
-  it('should exist on the apos object', function(done) {
-    apos = require('../index.js')({
+  it('should exist on the apos object', async function() {
+    apos = await require('../index.js')({
       root: module,
       shortName: 'test',
-
-      afterInit: function(callback) {
-        assert(apos.caches);
-        apos.argv._ = [];
-        return callback(null);
-      },
-
-      afterListen: function(err) {
-        console.error(err);
-        assert(!err);
-        return done();
+      argv: {
+        _: []
       }
     });
+    assert(apos.caches);
   });
   it('should give us a cache object', function() {
     cache = apos.caches.get('testMonkeys');
   });
-  it('should not crash on clear', function(done) {
-    cache.clear(done);
+  it('should not crash on clear', async function() {
+    await cache.clear();
   });
-  it('should not contain capuchin yet', function(done) {
-    return cache.get('capuchin', function(err, monkey) {
-      assert(!err);
-      assert(!monkey);
-      return done();
-    });
+  it('should not contain capuchin yet', async function() {
+    assert(!(await cache.get('capuchin')));
   });
-  it('should allow us to store capuchin', function(done) {
-    return cache.set('capuchin', { message: 'eek eek' }, function(err) {
-      assert(!err);
-      return done();
-    });
+  it('should allow us to store capuchin', async function() {
+    await cache.set('capuchin', { message: 'eek eek' });
   });
-  it('should now contain capuchin', function(done) {
-    return cache.get('capuchin', function(err, monkey) {
-      assert(!err);
-      assert(monkey);
-      assert(monkey.message === 'eek eek');
-      return done();
-    });
+  it('should now contain capuchin', async function() {
+    const monkey = await cache.get('capuchin');
+    assert(monkey);
+    assert(monkey.message === 'eek eek');
   });
-  it('should not crash on clear #2', function(done) {
-    cache.clear(done);
+  it('should not crash on clear #2', async function() {
+    await cache.clear();
   });
-  it('should not contain capuchin anymore', function(done) {
-    return cache.get('capuchin', function(err, monkey) {
-      assert(!err);
-      assert(!monkey);
-      return done();
-    });
-  });
-
-  it('should not crash on clear with promise', function() {
-    return cache.clear();
-  });
-  it('should not contain capuchin yet', function() {
-    return cache.get('capuchin')
-      .then(function(monkey) {
-        assert(!monkey);
-        return true;
-      });
-  });
-  it('should allow us to store capuchin', function() {
-    return cache.set('capuchin', { message: 'eek eek' });
-  });
-  it('should now contain capuchin', function() {
-    return cache.get('capuchin')
-      .then(function(monkey) {
-        assert(monkey);
-        assert(monkey.message === 'eek eek');
-        return true;
-      });
-  });
-  it('should allow us to store a value with a lifetime using a promise', function() {
-    return cache.set('colobus', { message: 'oop oop' }, 86400);
-  });
-  it('should now contain colobus', function() {
-    return cache.get('colobus')
-      .then(function(monkey) {
-        assert(monkey);
-        assert(monkey.message === 'oop oop');
-        return true;
-      });
-  });
-  it('should not crash on clear #2', function() {
-    return cache.clear();
-  });
-  it('should not contain capuchin anymore', function() {
-    return cache.get('capuchin')
-      .then(function(monkey) {
-        assert(!monkey);
-        return true;
-      });
+  it('should not contain capuchin anymore', async function() {
+    assert(!(await cache.get('capuchin')));
   });
 });
