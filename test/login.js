8a290853319ea204184eef5ced4af1ae8ff913ab
Alex Bea <alex.bea@gmail.com>
2019-05-10 16:25:27 -0400

    Convert first login tests

--- test/login.js
+++ test/login.js
@@ -1,6 +1,6 @@
-let t = require('../test-lib/test.js');
-let assert = require('assert');
-let request = require('request');
+const t = require('../test-lib/test.js');
+const assert = require('assert');
+const request = require('request-promise');
 
 let apos;
 
@@ -8,43 +8,43 @@ describe('Login', function() {
 
   this.timeout(20000);
 
-  after(function(done) {
-    return t.destroy(apos, done);
+  after(function() {
+    return t.destroy(apos);
   });
 
   // EXISTENCE
 
-  it('should initialize', function(done) {
-    apos = require('../index.js')({
+  it('should initialize', async function() {
+    apos = await require('../index.js')({
       root: module,
       shortName: 'test',
+      argv: {
+        _: []
+      },
       modules: {
         'apostrophe-express': {
-          secret: 'xxx',
           port: 7901,
-          csrf: false
+          csrf: false,
+          address: 'localhost',
+          session: {
+            secret: 'Cursus'
+          }
         }
       },
       afterInit: function(callback) {
-        assert(apos.modules['apostrophe-login']);
-        apos.argv._ = [];
-        assert(apos.users.safe.remove);
-        return apos.users.safe.remove({}, callback);
         // return callback(null);
-      },
-      afterListen: function(err) {
-        if (err) {
-          console.error('* * * caught error ', err);
-        }
-        assert(!err);
-        done();
       }
     });
+
+    assert(apos.modules['apostrophe-login']);
+    assert(apos.users.safe.remove);
+    const response = await apos.users.safe.remove({});
+    assert(response.result.ok === 1);
   });
 
-  it('should be able to insert test user', function(done) {
+  it('should be able to insert test user', async function() {
     assert(apos.users.newInstance);
-    let user = apos.users.newInstance();
+    const user = apos.users.newInstance();
     assert(user);
 
     user.firstName = 'Harry';
@@ -56,90 +56,89 @@ describe('Login', function() {
 
     assert(user.type === 'apostrophe-user');
     assert(apos.users.insert);
-    apos.users.insert(apos.tasks.getReq(), user, function(err) {
-      assert(!err);
-      done();
-    });
+    const doc = await apos.users.insert(apos.tasks.getReq(), user);
+    assert(doc._id);
   });
 
-  it('should not see logout link yet', function(done) {
+  it('should not see logout link yet', async function() {
     // otherwise logins are not remembered in a session
     request.jar();
-    return request('http://localhost:7901/', function(err, response, body) {
-      assert(!err);
-      // Is our status code good?
-      assert.equal(response.statusCode, 200);
-      // Did we get our page back?
-      assert(body.match(/login/));
-      assert(!body.match(/logout/));
-      return done();
+
+    const response = await request({
+      uri: 'http://localhost:7901/',
+      resolveWithFullResponse: true
     });
 
+    // Is our status code good?
+    assert.strictEqual(response.statusCode, 200);
+    // Did we get our page back?
+    assert(response.body.match(/login/));
+    assert(!response.body.match(/logout/));
   });
 
-  let loginLogoutJar = request.jar();
-  let loginEmailLogoutJar = request.jar();
+  // const loginLogoutJar = request.jar();
+  // const loginEmailLogoutJar = request.jar();
 
-  it('should be able to login a user', function(done) {
-    // otherwise logins are not remembered in a session
-    return request.post('http://localhost:7901/login', {
-      form: { username: 'HarryPutter', password: 'crookshanks' },
-      followAllRedirects: true,
-      jar: loginLogoutJar
-    }, function(err, response, body) {
-      assert(!err);
-      // Is our status code good?
-      assert.equal(response.statusCode, 200);
-      // Did we get our page back?
-      assert(body.match(/logout/));
-      return done();
-    });
-  });
+  // it('should be able to login a user', function(done) {
+  //   // otherwise logins are not remembered in a session
+  //   return request.post('http://localhost:7901/login', {
+  //     form: { username: 'HarryPutter', password: 'crookshanks' },
+  //     followAllRedirects: true,
+  //     jar: loginLogoutJar
+  //   }, function(err, response, body) {
+  //     assert(!err);
+  //     // Is our status code good?
+  //     assert.equal(response.statusCode, 200);
+  //     // Did we get our page back?
+  //     assert(body.match(/logout/));
+  //     return done();
+  //   });
+  // });
 
-  it('should be able to login a user with their email', function(done) {
-    // otherwise logins are not remembered in a session
-    return request.post('http://localhost:7901/login', {
-      form: { username: 'hputter@aol.com', password: 'crookshanks' },
-      followAllRedirects: true,
-      jar: loginEmailLogoutJar
-    }, function(err, response, body) {
-      assert(!err);
-      // Is our status code good?
-      assert.equal(response.statusCode, 200);
-      // Did we get our page back?
-      assert(body.match(/logout/));
-      return done();
-    });
-  });
+  // it('should be able to login a user with their email', function(done) {
+  //   // otherwise logins are not remembered in a session
+  //   return request.post('http://localhost:7901/login', {
+  //     form: { username: 'hputter@aol.com', password: 'crookshanks' },
+  //     followAllRedirects: true,
+  //     jar: loginEmailLogoutJar
+  //   }, function(err, response, body) {
+  //     assert(!err);
+  //     // Is our status code good?
+  //     assert.equal(response.statusCode, 200);
+  //     // Did we get our page back?
+  //     assert(body.match(/logout/));
+  //     return done();
+  //   });
+  // });
 
-  it('should be able to log out', function(done) {
-    // otherwise logins are not remembered in a session
-    return request('http://localhost:7901/logout', {
-      followAllRedirects: true,
-      jar: loginLogoutJar
-    }, function(err, response, body) {
-      assert(!err);
-      // Is our status code good?
-      assert.equal(response.statusCode, 200);
-      // are we back to being able to log in?
-      assert(body.match(/login/));
-      return done();
-    });
-  });
+  // it('should be able to log out', function(done) {
+  //   // otherwise logins are not remembered in a session
+  //   return request('http://localhost:7901/logout', {
+  //     followAllRedirects: true,
+  //     jar: loginLogoutJar
+  //   }, function(err, response, body) {
+  //     assert(!err);
+  //     // Is our status code good?
+  //     assert.equal(response.statusCode, 200);
+  //     // are we back to being able to log in?
+  //     assert(body.match(/login/));
+  //     return done();
+  //   });
+  // });
 
-  it('should be able to log out after having logged in with email', function(done) {
-    // otherwise logins are not remembered in a session
-    return request('http://localhost:7901/logout', {
-      followAllRedirects: true,
-      jar: loginEmailLogoutJar
-    }, function(err, response, body) {
-      assert(!err);
-      // Is our status code good?
-      assert.equal(response.statusCode, 200);
-      // are we back to being able to log in?
-      assert(body.match(/login/));
-      return done();
-    });
-  });
+  // it('should be able to log out after having logged in with email', function(done) {
+  //   // otherwise logins are not remembered in a session
+  //   return request('http://localhost:7901/logout', {
+  //     followAllRedirects: true,
+  //     jar: loginEmailLogoutJar
+  //   }, function(err, response, body) {
+  //     assert(!err);
+  //     // Is our status code good?
+  //     assert.equal(response.statusCode, 200);
+  //     // are we back to being able to log in?
+  //     assert(body.match(/login/));
+  //     return done();
+  //   });
+  // });
 
 });
