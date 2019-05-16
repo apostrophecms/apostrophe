4fbadc0ab13e9af2f6b0ed00d10aaa37bd2f8365
Alex Bea <alex.bea@gmail.com>
2019-05-16 14:32:47 -0400

    Converts tests to async

--- test/pages.js
+++ test/pages.js
@@ -80,233 +80,215 @@ describe('Pages', function() {
     assert(home.published);
   });
 
-  // it('parked trash can exists', function() {
-  //   return apos.pages.find(apos.tasks.getReq(), { slug: '/trash' }).published(null).trash(null).toObject(function(err, trash) {
-  //     assert(!err);
-  //     assert(trash);
-  //     assert(trash.slug === '/trash');
-  //     assert(trash.path === '/trash');
-  //     assert(trash.type === 'trash');
-  //     assert(trash.parked);
-  //     assert(!trash.published);
-  //     // Verify that clonePermanent did its
-  //     // job and removed properties not meant
-  //     // to be stored in mongodb
-  //     assert(!trash._children);
-  //     done();
-  //   });
-  // });
+  it('parked trash can exists', async function() {
+    const trash = await apos.pages.find(apos.tasks.getReq(), { slug: '/trash' }).published(null).trash(null).toObject();
 
-  // it('should be able to use db to insert documents', function() {
-  //   const testItems = [
-  //     { _id: '1234',
-  //       type: 'testPage',
-  //       slug: '/parent',
-  //       published: true,
-  //       path: '/parent',
-  //       level: 1,
-  //       rank: 0
-  //     },
-  //     {
-  //       _id: '2341',
-  //       type: 'testPage',
-  //       slug: '/child',
-  //       published: true,
-  //       path: '/parent/child',
-  //       level: 2,
-  //       rank: 0
-  //     },
-  //     {
-  //       _id: '4123',
-  //       type: 'testPage',
-  //       slug: '/grandchild',
-  //       published: true,
-  //       path: '/parent/child/grandchild',
-  //       level: 3,
-  //       rank: 0
-  //     },
-  //     {
-  //       _id: '4321',
-  //       type: 'testPage',
-  //       slug: '/sibling',
-  //       published: true,
-  //       path: '/parent/sibling',
-  //       level: 2,
-  //       rank: 1
+    assert(trash);
+    assert(trash.slug === '/trash');
+    assert(trash.path === '/trash');
+    assert(trash.type === 'trash');
+    assert(trash.parked);
+    assert(!trash.published);
+    // Verify that clonePermanent did its
+    // job and removed properties not meant
+    // to be stored in mongodb
+    assert(!trash._children);
+  });
 
-  //     },
-  //     {
-  //       _id: '4312',
-  //       type: 'testPage',
-  //       slug: '/cousin',
-  //       published: true,
-  //       path: '/parent/sibling/cousin',
-  //       level: 3,
-  //       rank: 0
-  //     },
-  //     {
-  //       _id: '4333',
-  //       type: 'testPage',
-  //       slug: '/another-parent',
-  //       published: true,
-  //       path: '/another-parent',
-  //       level: 1,
-  //       rank: 0
-  //     }
-  //   ];
+  it('should be able to use db to insert documents', async function() {
+    const testItems = [
+      { _id: '1234',
+        type: 'testPage',
+        slug: '/parent',
+        published: true,
+        path: '/parent',
+        level: 1,
+        rank: 0
+      },
+      {
+        _id: '2341',
+        type: 'testPage',
+        slug: '/child',
+        published: true,
+        path: '/parent/child',
+        level: 2,
+        rank: 0
+      },
+      {
+        _id: '4123',
+        type: 'testPage',
+        slug: '/grandchild',
+        published: true,
+        path: '/parent/child/grandchild',
+        level: 3,
+        rank: 0
+      },
+      {
+        _id: '4321',
+        type: 'testPage',
+        slug: '/sibling',
+        published: true,
+        path: '/parent/sibling',
+        level: 2,
+        rank: 1
 
-  //   apos.docs.db.insert(testItems, function(err) {
-  //     assert(!err);
-  //     done();
-  //   });
+      },
+      {
+        _id: '4312',
+        type: 'testPage',
+        slug: '/cousin',
+        published: true,
+        path: '/parent/sibling/cousin',
+        level: 3,
+        rank: 0
+      },
+      {
+        _id: '4333',
+        type: 'testPage',
+        slug: '/another-parent',
+        published: true,
+        path: '/another-parent',
+        level: 1,
+        rank: 0
+      }
+    ];
 
-  // });
+    const items = await apos.docs.db.insert(testItems);
 
-  // // FINDING
+    assert(items.result.ok === 1);
+    assert(items.insertedCount === 6);
+  });
 
-  // it('should have a find method on pages that returns a cursor', function() {
-  //   const cursor = apos.pages.find(apos.tasks.getAnonReq());
-  //   assert(cursor);
-  // });
+  // FINDING
 
-  // it('should be able to find the parked homepage', function() {
-  //   const cursor = apos.pages.find(apos.tasks.getAnonReq(), { slug: '/' });
+  it('should have a find method on pages that returns a cursor', async function() {
+    const cursor = apos.pages.find(apos.tasks.getAnonReq());
+    assert(cursor);
+  });
 
-  //   cursor.toObject(function(err, page) {
-  //     assert(!err);
-  //     // There should be only 1 result.
-  //     assert(page);
-  //     // It should have a path of /
-  //     assert(page.path === '/');
-  //     assert(page.rank === 0);
-  //     done();
-  //   });
-  // });
+  it('should be able to find the parked homepage', async function() {
+    const cursor = apos.pages.find(apos.tasks.getAnonReq(), { slug: '/' });
 
-  // it('should be able to find just a single page', function() {
-  //   const cursor = apos.pages.find(apos.tasks.getAnonReq(), { slug: '/child' });
+    const page = await cursor.toObject();
 
-  //   cursor.toObject(function(err, page) {
-  //     assert(!err);
-  //     // There should be only 1 result.
-  //     assert(page);
-  //     // It should have a path of /parent/child
-  //     assert(page.path === '/parent/child');
-  //     done();
-  //   });
-  // });
+    // There should be only 1 result.
+    assert(page);
+    // It should have a path of /
+    assert(page.path === '/');
+    assert(page.rank === 0);
+  });
 
-  // it('should be able to include the ancestors of a page', function() {
-  //   const cursor = apos.pages.find(apos.tasks.getAnonReq(), { slug: '/child' });
+  it('should be able to find just a single page', async function() {
+    const cursor = apos.pages.find(apos.tasks.getAnonReq(), { slug: '/child' });
 
-  //   cursor.ancestors(true).toObject(function(err, page) {
-  //     assert(!err);
-  //     // There should be only 1 result.
-  //     assert(page);
-  //     // There should be 2 ancestors.
-  //     assert(page._ancestors.length === 2);
-  //     // The first ancestor should be the homepage
-  //     assert.strictEqual(page._ancestors[0].path, '/');
-  //     // The second ancestor should be 'parent'
-  //     assert.strictEqual(page._ancestors[1].path, '/parent');
-  //     done();
-  //   });
-  // });
+    const page = await cursor.toObject();
 
-  // it('should be able to include just one ancestor of a page, i.e. the parent', function() {
-  //   const cursor = apos.pages.find(apos.tasks.getAnonReq(), { slug: '/child' });
+    // There should be only 1 result.
+    assert(page);
+    // It should have a path of /parent/child
+    assert(page.path === '/parent/child');
+  });
 
-  //   cursor.ancestors({ depth: 1 }).toObject(function(err, page) {
-  //     assert(!err);
-  //     // There should be only 1 result.
-  //     assert(page);
-  //     // There should be 1 ancestor returned.
-  //     assert(page._ancestors.length === 1);
-  //     // The first ancestor returned should be 'parent'
-  //     assert.strictEqual(page._ancestors[0].path, '/parent');
-  //     done();
-  //   });
-  // });
+  it('should be able to include the ancestors of a page', async function() {
+    const cursor = apos.pages.find(apos.tasks.getAnonReq(), { slug: '/child' });
 
-  // it('should be able to include the children of the ancestors of a page', function() {
-  //   const cursor = apos.pages.find(apos.tasks.getAnonReq(), { slug: '/child' });
+    const page = await cursor.ancestors(true).toObject();
 
-  //   cursor.ancestors({ children: 1 }).toObject(function(err, page) {
-  //     assert(!err);
-  //     // There should be only 1 result.
-  //     assert(page);
-  //     // There should be 2 ancestors.
-  //     assert(page._ancestors.length === 2);
-  //     // The second ancestor should have children
-  //     assert(page._ancestors[1]._children);
-  //     // The first ancestor's child should have a path '/parent/child'
-  //     assert.strictEqual(page._ancestors[1]._children[0].path, '/parent/child');
-  //     // The second ancestor's child should have a path '/parent/sibling'
-  //     assert.strictEqual(page._ancestors[1]._children[1].path, '/parent/sibling');
-  //     done();
-  //   });
-  // });
+    // There should be only 1 result.
+    assert(page);
+    // There should be 2 ancestors.
+    assert(page._ancestors.length === 2);
+    // The first ancestor should be the homepage
+    assert.strictEqual(page._ancestors[0].path, '/');
+    // The second ancestor should be 'parent'
+    assert.strictEqual(page._ancestors[1].path, '/parent');
+  });
 
-  // // INSERTING
+  it('should be able to include just one ancestor of a page, i.e. the parent', async function() {
+    const cursor = apos.pages.find(apos.tasks.getAnonReq(), { slug: '/child' });
 
-  // it('is able to insert a new page', function() {
-  //   const parentId = '1234';
+    const page = await cursor.ancestors({ depth: 1 }).toObject();
 
-  //   const newPage = {
-  //     slug: '/new-page',
-  //     published: true,
-  //     type: 'testPage',
-  //     title: 'New Page'
-  //   };
-  //   apos.pages.insert(apos.tasks.getReq(), parentId, newPage, function(err, page) {
-  //     // did it return an error?
-  //     assert(!err);
-  //     // Is the path generally correct?
-  //     assert.strictEqual(page.path, '/parent/new-page');
-  //     done();
-  //   });
-  // });
+    // There should be only 1 result.
+    assert(page);
+    // There should be 1 ancestor returned.
+    assert(page._ancestors.length === 1);
+    // The first ancestor returned should be 'parent'
+    assert.strictEqual(page._ancestors[0].path, '/parent');
+  });
 
-  // it('is able to insert a new page in the correct order', function() {
-  //   const cursor = apos.pages.find(apos.tasks.getAnonReq(), { slug: '/new-page' });
+  it('should be able to include the children of the ancestors of a page', async function() {
+    const cursor = apos.pages.find(apos.tasks.getAnonReq(), { slug: '/child' });
 
-  //   cursor.toObject(function(err, page) {
-  //     assert(!err);
-  //     assert(page);
-  //     assert.strictEqual(page.rank, 2);
-  //     done();
-  //   });
-  // });
+    const page = await cursor.ancestors({ children: 1 }).toObject();
 
-  // // INSERTING
+    // There should be only 1 result.
+    assert(page);
+    // There should be 2 ancestors.
+    assert(page._ancestors.length === 2);
+    // The second ancestor should have children
+    assert(page._ancestors[1]._children);
+    // The first ancestor's child should have a path '/parent/child'
+    assert.strictEqual(page._ancestors[1]._children[0].path, '/parent/child');
+    // The second ancestor's child should have a path '/parent/sibling'
+    assert.strictEqual(page._ancestors[1]._children[1].path, '/parent/sibling');
+  });
 
-  // it('is able to insert a new page with promises', function() {
-  //   const parentId = '1234';
+  // INSERTING
 
-  //   const newPage = {
-  //     slug: '/new-page-2',
-  //     published: true,
-  //     type: 'testPage',
-  //     title: 'New Page 2'
-  //   };
-  //   apos.pages.insert(apos.tasks.getReq(), parentId, newPage).then(function(page) {
-  //     assert.strictEqual(page.path, '/parent/new-page-2');
-  //     done();
-  //   }).catch(function(err) {
-  //     assert(!err);
-  //   });
-  // });
+  it('is able to insert a new page', async function() {
+    const parentId = '1234';
 
-  // it('is able to insert a new page in the correct order with promises', function() {
-  //   const cursor = apos.pages.find(apos.tasks.getAnonReq(), { slug: '/new-page-2' });
+    const newPage = {
+      slug: '/new-page',
+      published: true,
+      type: 'testPage',
+      title: 'New Page'
+    };
 
-  //   cursor.toObject().then(function(page) {
-  //     assert.strictEqual(page.rank, 3);
-  //     done();
-  //   }).catch(function(err) {
-  //     assert(!err);
-  //   });
-  // });
+    const page = await apos.pages.insert(apos.tasks.getReq(), parentId, newPage);
+
+    // Is the path generally correct?
+    assert.strictEqual(page.path, '/parent/new-page');
+  });
+
+  it('is able to insert a new page in the correct order', async function() {
+    const cursor = apos.pages.find(apos.tasks.getAnonReq(), {
+      slug: '/new-page'
+    });
+
+    const page = await cursor.toObject();
+
+    assert(page);
+    assert.strictEqual(page.rank, 2);
+  });
+
+  // INSERTING
+
+  it('is able to insert a new page with promises', async function() {
+    const parentId = '1234';
+
+    const newPage = {
+      slug: '/new-page-2',
+      published: true,
+      type: 'testPage',
+      title: 'New Page 2'
+    };
+
+    const page = await apos.pages.insert(apos.tasks.getReq(), parentId, newPage);
+
+    assert.strictEqual(page.path, '/parent/new-page-2');
+  });
+
+  it('is able to insert a new page in the correct order with promises', async function() {
+    const cursor = apos.pages.find(apos.tasks.getAnonReq(), {
+      slug: '/new-page-2'
+    });
+
+    const page = await cursor.toObject();
+
+    assert.strictEqual(page.rank, 3);
+  });
 
   // // MOVING
 
