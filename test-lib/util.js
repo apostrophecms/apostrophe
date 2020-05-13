9955b49c4d4a686658deb0d56306b11f58bf2f41
Tom Boutell <tom@punkave.com>
2020-05-13 14:36:30 -0400

    don't drop the db unless we opened it

--- test-lib/util.js
+++ test-lib/util.js
@@ -7,7 +7,7 @@
 // If `apos` is null, no work is done.
 
 async function destroy(apos) {
-  const dbName = apos.db.databaseName;
+  const dbName = apos.db && apos.db.databaseName;
   if (apos) {
     await apos.destroy();
   }
@@ -16,7 +16,9 @@ async function destroy(apos) {
   // important principle here is that we should not have to have an apos
   // object to clean up the database, otherwise we have to get hold of one
   // when initialization failed and that's really not apostrophe's concern
-  require('child_process').execSync(`mongo ${dbName} --eval 'db.dropDatabase()'`);
+  if (dbName) {
+    require('child_process').execSync(`mongo ${dbName} --eval 'db.dropDatabase()'`);
+  }
 };
 
 module.exports = {
