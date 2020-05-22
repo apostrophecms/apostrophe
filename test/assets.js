fc2531f027396c8a35b442fe9e484bb7be204e14
Tom Boutell <tom@punkave.com>
2020-05-22 09:16:41 -0400

    express static middleware test passing

---
+++ test/assets.js
@@ -0,0 +1,30 @@
+const t = require('../test-lib/test.js');
+const assert = require('assert');
+let apos;
+
+describe('Assets', function() {
+
+  after(async function() {
+    return t.destroy(apos);
+  });
+
+  this.timeout(t.timeout);
+
+  it('should should exist on the apos object', async function() {
+    apos = await t.create({
+      root: module,
+      modules: {
+        'express-test': {},
+        'templates-test': {},
+        'templates-subclass-test': {}
+      }
+    });
+    assert(apos.assets);
+  });
+
+  it('should serve static files', async function() {
+    const text = await apos.http.get('/static-test.txt');
+    assert(text.match(/served/));
+  });
+
+});
