71d8696b2ea279b401dded87c54bf1984f8884ac
Alex Bea <abea@mailbox.org>
2020-06-25 11:11:35 -0500

    Renaming and cleans up TheAposModals

--- lib/modules/@apostrophecms/modals/src/apos/components/TheAposModals.vue
+++ lib/modules/@apostrophecms/modals/src/apos/components/TheAposModals.vue
@@ -1,31 +1,44 @@
 <template>
   <div id="apos-modals">
-    <component v-for="modal in modals" :is="modal.componentName" v-if="active[modal.itemName]" @close="setIsActive(modal.itemName, false)" :moduleName="modal.itemName">
-    </component>
+    <component
+      v-for="modal in activeModals" :key="modal.itemName"
+      :is="modal.componentName" :module-name="modal.itemName"
+      @close="setIsActive(modal.itemName, false)"
+    />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TheApostropheModals',
+  name: 'TheAposModals',
+  props: {
+    modals: {
+      type: Array,
+      required: true
+    }
+  },
   data() {
     return {
       active: {}
+    };
+  },
+  computed: {
+    activeModals() {
+      return this.modals.filter(modal => {
+        return this.active[modal.itemName];
+      });
     }
+  },
+  mounted() {
+    apos.bus.$on('adminBarItem', (itemName) => {
+      this.setIsActive(itemName, true);
+    });
   },
   methods: {
     setIsActive(itemName, state) {
       // https://vuejs.org/v2/guide/reactivity.html#Change-Detection-Caveats
       this.$set(this.active, itemName, state);
     }
-  },
-  props: {
-    modals: Array
-  },
-  mounted() {
-    apos.bus.$on('adminBarItem', (itemName) => {
-      this.setIsActive(itemName, true);
-    });
   }
 };
 </script>
