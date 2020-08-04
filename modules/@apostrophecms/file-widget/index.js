0cebe625bf1722b3456375435b61a6500451915e
Tom Boutell <tom@punkave.com>
2020-08-04 11:14:48 -0400

    purge old templates and modules

--- modules/@apostrophecms/file-widget/index.js
+++
@@ -1,7 +0,0 @@
-// Provides widgets for downloading PDF files and other files that have been
-// uploaded to the file library provided by [@apostrophecms/file](../@apostrophecms/file/index.html).
-
-module.exports = {
-  extend: '@apostrophecms/piece-widget-type',
-  options: { label: 'File(s)' }
-};
