2a32b3027166d9c046daea38e1d2ef0e659af33e
Anthony Tarlao <anthony.tarlao@gmail.com>
2020-08-19 17:05:59 +0200

    Date field

---
+++ modules/@apostrophecms/schema/ui/apos/components/AposInputDate.vue
@@ -0,0 +1,27 @@
+<template>
+  <AposInputString
+    type="date"
+    @input="input"
+    :field="field"
+    :status="status"
+    :value="value"
+  />
+</template>
+
+<script>
+import AposInputMixin from '../mixins/AposInputMixin';
+
+export default {
+  name: 'AposInputDate',
+  mixins: [ AposInputMixin ],
+  methods: {
+    input(value) {
+      this.next = value.data;
+      this.error = value.error;
+    },
+    validate() {
+      return false;
+    }
+  }
+};
+</script>
