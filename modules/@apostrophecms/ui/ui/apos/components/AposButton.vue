e12ddfbd2b601dcf63469aa6fb9ab352baeb373d
Tom Boutell <tom@apostrophecms.com>
2020-09-11 08:43:32 -0400

    Make sure storybook plays by these new rules too

--- modules/@apostrophecms/ui/ui/apos/components/AposButton.vue
+++ modules/@apostrophecms/ui/ui/apos/components/AposButton.vue
@@ -137,7 +137,7 @@ export default {
 };
 </script>
 
-<!-- Unscoped because the admin bar overrides styles for these when nested-->
+<!-- Unscoped because the admin bar overrides styles for these when nested -->
 <style lang="scss">
 
 .apos-button {
