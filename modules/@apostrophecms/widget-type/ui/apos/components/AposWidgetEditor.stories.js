b6581e6f4a1cdbb3e52ecc0e773cc4fa68e8bd04
Tom Boutell <tom@apostrophecms.com>
2020-10-20 09:36:20 -0400

    getData is dead code

--- modules/@apostrophecms/widget-type/ui/apos/components/AposWidgetEditor.stories.js
+++ modules/@apostrophecms/widget-type/ui/apos/components/AposWidgetEditor.stories.js
@@ -56,33 +56,3 @@ export const widgetEditor = () => {
     }
   };
 };
-
-function getData() {
-  return {
-    doc: {
-      metaText: 'Outrage over police injustice is older than the nation',
-      title: 'Porta Magna Egestas',
-      description: 'A tip of the internet is assumed to be an unlet baritone. The literature would have us believe that a castled anthony is not but a jaw. Few can name a rimless cook that isn'
-    },
-    schema: [
-      {
-        name: 'metaText',
-        label: 'Meta Text',
-        type: 'string'
-      },
-      {
-        name: 'title',
-        label: 'Title',
-        type: 'string',
-        required: true
-      },
-      {
-        name: 'description',
-        label: 'Description',
-        type: 'string',
-        textarea: true
-      }
-    ]
-  };
-
-}
