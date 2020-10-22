8813814fe79aa5a36a4e4a3c5e717f58f2cfce32
Alex Bea <abea@mailbox.org>
2020-10-22 17:23:45 -0500

    Restores the group method to admin menu

--- modules/@apostrophecms/admin-bar/index.js
+++ modules/@apostrophecms/admin-bar/index.js
@@ -103,6 +103,17 @@ module.exports = {
         self.items.push(item);
       },
 
+      // Group several menu items together in the interface (currently
+      // implemented as a dropdown menu). If `items` is an array of menu
+      // item names, then the group's label is the same as the label of
+      // the first item. If you wish the label to differ from the label
+      // of the first item, instead pass an object with a `label` property
+      // and an `items` property.
+
+      group(items) {
+        self.groups.push(items);
+      },
+
       getVisibleItems(req) {
         // Find the subset of admin bar items this user is permitted to see
         const user = req.user;
