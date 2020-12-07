ca3d25ab5ac5a0780f3ff0bbc041c9d5a10936a3
Alex Bea <abea@mailbox.org>
2020-12-07 17:12:36 -0600

    Uses cell components for piece manager display

---
+++ modules/@apostrophecms/ui/ui/apos/components/AposCellBasic.vue
@@ -0,0 +1,24 @@
+<template>
+  <p
+    class="apos-table__cell-field"
+    :class="`apos-table__cell-field--${header.name}`"
+  >
+    {{ item[header.name] }}
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'AposCellBasic',
+  props: {
+    item: {
+      type: Object,
+      required: true
+    },
+    header: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
