eb835523ee5ab985c0eada572e524e3092193e9d
Alex Bea <alex.bea@gmail.com>
2020-12-29 14:16:29 -0600

    Updates pager to allow flexible number of intermediate pgs

--- modules/@apostrophecms/pager/index.js
+++ modules/@apostrophecms/pager/index.js
@@ -9,14 +9,38 @@ module.exports = {
       // Just a little too much math to be comfortable in pure Nunjucks
       pageRange: function (options) {
         const pages = [];
-        let fromPage = options.page - 2;
+        let fromPage = options.page - (Math.floor(options.shown / 2));
+
+        if (fromPage > options.total - options.shown) {
+          fromPage = options.total - options.shown;
+        }
+
         if (fromPage < 2) {
           fromPage = 2;
         }
-        for (let page = fromPage; page < fromPage + options.shown; page++) {
-          pages.push(page);
+
+        for (let pg = fromPage; pg < fromPage + options.shown; pg++) {
+          if (pg < options.total) {
+            pages.push(pg);
+          }
         }
         return pages;
+      },
+      showHeadGap: function (options) {
+        if (options.shown % 2 === 0) {
+          return ((options.page - (Math.floor(options.shown / 2))) > 2)
+            && options.page > (options.shown - Math.floor(options.shown / 2));
+        } else {
+          return ((options.page - (Math.floor(options.shown / 2))) > 2)
+            && (options.page >= (options.shown - Math.floor(options.shown / 2)));
+        }
+      },
+      showTailGap(options) {
+        if (options.shown % 2 === 0) {
+          return options.page < (options.total - (options.shown - 2));
+        } else {
+          return options.page <= (options.total - (options.shown - 2));
+        }
       }
     };
   }
