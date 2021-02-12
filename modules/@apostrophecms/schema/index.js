6fbed53f3a98ce2e318291ffd90101c7c886dec7
Alex Bea <abea@mailbox.org>
2021-02-12 15:18:11 -0600

    Fixes malformed error

--- modules/@apostrophecms/schema/index.js
+++ modules/@apostrophecms/schema/index.js
@@ -91,7 +91,7 @@ module.exports = {
         // This is new and until now if JS client side failed, then it would
         // allow the save with empty values -Lars
         if (field.required && (_.isUndefined(data[field.name]) || !data[field.name].toString().length)) {
-          throw self.apos.error(`required: ${field.name}`);
+          throw self.apos.error('required');
         }
       },
       index: function (value, field, texts) {
