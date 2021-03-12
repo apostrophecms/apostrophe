7fb1f433c533ea783aec6e6fc524e2e21573cb47
Tom Boutell <tom@apostrophecms.com>
2021-03-12 14:35:29 -0500

    fixes bug where images, other relationships do not appear in the editor after selection

--- modules/@apostrophecms/widget-type/ui/apos/mixins/AposWidgetMixin.js
+++ modules/@apostrophecms/widget-type/ui/apos/mixins/AposWidgetMixin.js
@@ -52,7 +52,7 @@ export default {
           this.runPlayer();
         } else {
           this.rendered = '...';
-          this.rendered = await apos.http.post(`${apos.area.action}/render-widget?apos-edit=1`, {
+          this.rendered = await apos.http.post(`${apos.area.action}/render-widget?apos-edit=1&apos-mode=draft`, {
             busy: true,
             body: parameters
           });
