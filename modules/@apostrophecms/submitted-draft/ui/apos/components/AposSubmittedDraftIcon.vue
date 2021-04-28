d8874ed6f5c658dc92480c7e6fcbd95efc436858
Stuart Romanek <stuart@apostrophecms.com>
2021-04-27 20:24:00 -0400

    wip

--- modules/@apostrophecms/submitted-draft/ui/apos/components/AposSubmittedDraftIcon.vue
+++ modules/@apostrophecms/submitted-draft/ui/apos/components/AposSubmittedDraftIcon.vue
@@ -1,11 +1,11 @@
 <template>
-  <span v-if="count > 0" class="apos-submitted-drafts__container">
+  <span class="apos-submitted-drafts__container" :class="count < 1 ? 'apos-submitted-drafts__container--disabled' : ''">
     <AposIndicator
       icon="tray-full-icon"
       class="apos-submitted-drafts__icon"
       :icon-size="size"
     />
-    <span v-if="canPublish" class="apos-submitted-drafts__counter">
+    <span v-if="canPublish && count > 0" class="apos-submitted-drafts__counter">
       {{ count }}
     </span>
   </span>
@@ -54,3 +54,41 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+  .apos-submitted-drafts__container {
+    display: inline-flex;
+    align-content: center;
+    justify-items: space-between;
+    &:hover {
+      /deep/ .apos-submitted-drafts__icon svg {
+        fill: var(--a-primary);
+      }
+    }
+  }
+
+  .apos-submitted-drafts__container--disabled {
+    &:hover {
+      cursor: not-allowed;
+      pointer-events: none;
+    }
+    & /deep/ .apos-submitted-drafts__icon svg {
+      fill: var(--a-base-6);
+    }
+  }
+
+  .apos-submitted-drafts__counter {
+    display: inline-flex;
+    margin-left: $spacing-half;
+    padding: 3px;
+    background-color: var(--a-primary-10);
+    border-radius: var(--a-border-radius);
+    color: var(--a-primary);
+    min-width: 15px;
+    min-height: 15px;
+    align-items: center;
+    justify-content: center;
+    font-size: var(--a-type-small);
+    line-height: 0.9;
+  }
+</style>
