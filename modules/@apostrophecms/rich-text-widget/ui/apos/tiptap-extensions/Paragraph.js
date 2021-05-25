89a3a06c86d81706d7e5f28003ffa1e6c4de0b23
Stuart Romanek <stuart@apostrophecms.com>
2021-05-25 11:32:28 -0400

    ugh

--- modules/@apostrophecms/rich-text-widget/ui/apos/tiptap-extensions/Paragraph.js
+++ modules/@apostrophecms/rich-text-widget/ui/apos/tiptap-extensions/Paragraph.js
@@ -1,19 +1,39 @@
 import Paragraph from '@tiptap/extension-paragraph';
 export default (options) => {
-  console.log('hello');
-  console.log(options);
+  const allow = options.styles
+    .filter(s => s.tag === 'p')
+    .map(s => s.class);
+  // console.log(classes);
   return Paragraph.extend({
     addAttributes() {
       return {
         class: {
-          default: 'def',
+          // default: allow.length ? allow[0] : null,
+          default: null,
           parseHTML(element) {
             return {
               class: element.getAttribute('class')
             };
+          },
+          renderHTML(attributes) {
+            console.log(attributes.class);
+            const classes = (attributes.class || '')
+              .split(' ')
+              .filter(c => {
+                // console.log(c);
+                // console.log(allowList.includes(c));
+                return allow.includes(c);
+              });
+            console.log(classes);
+            // const allowedClass = attributes.class.filter()
+            return {
+              // class: attributes.class
+              class: classes.length ? classes.join(' ') : null
+            };
           }
         }
       };
-    }
+    },
+
   });
 };
