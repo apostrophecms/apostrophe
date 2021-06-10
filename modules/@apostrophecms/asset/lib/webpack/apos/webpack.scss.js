84533807915080160343a8920a2dfbc82acbe7f4
Tom Boutell <tom@apostrophecms.com>
2021-06-10 08:44:52 -0400

    put the linting back to normal for apos build

--- modules/@apostrophecms/asset/lib/webpack/apos/webpack.scss.js
+++ modules/@apostrophecms/asset/lib/webpack/apos/webpack.scss.js
@@ -60,7 +60,7 @@ module.exports = (options, apos) => {
     },
     plugins: [
       new StyleLintPlugin({
-        files: [ './node_modules/apostrophe/modules/*/ui/apos/**/*.scss' ]
+        files: [ './node_modules/apostrophe/modules/**/*.{scss,vue}' ]
       })
     ]
   };
