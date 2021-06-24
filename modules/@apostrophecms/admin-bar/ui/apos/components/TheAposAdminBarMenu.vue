828a49a7139289ee43059c6f75553418a6a5f33b
Stuart Romanek <stuart@apostrophecms.com>
2021-06-24 11:16:49 -0400

    rename page tree to pages

--- modules/@apostrophecms/admin-bar/ui/apos/components/TheAposAdminBarMenu.vue
+++ modules/@apostrophecms/admin-bar/ui/apos/components/TheAposAdminBarMenu.vue
@@ -2,7 +2,7 @@
   <ul class="apos-admin-bar__items">
     <li class="apos-admin-bar__item" v-if="pageTree">
       <AposButton
-        type="subtle" label="Page Tree"
+        type="subtle" label="Pages"
         class="apos-admin-bar__btn"
         :modifiers="['no-motion']"
         @click="emitEvent('@apostrophecms/page:manager')"
