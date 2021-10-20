dd46c45bc1b9ebcab0922d2e834a1c0e371cbe4f
Jed <valentin.jeudy@gmail.com>
2021-10-20 18:17:02 +0200

    select box component style and  infos

--- modules/@apostrophecms/piece-type/ui/apos/components/AposDocsManagerSelectBox.vue
+++ modules/@apostrophecms/piece-type/ui/apos/components/AposDocsManagerSelectBox.vue
@@ -1,10 +1,17 @@
 <template>
   <transition
     name="collapse"
-    :duration="1000"
+    :duration="300"
   >
-    <div class="apos-select-box">
-      <h3>Select Box !!!</h3>
+    <div
+      v-if="selectedState === 'checked'"
+      class="apos-select-box"
+    >
+      <div class="apos-select-box__content">
+        <h3 class="apos-select-box__text">
+          {{ getItemsNumber }} on this page selected.
+        </h3>
+      </div>
     </div>
   </transition>
 </template>
@@ -16,28 +23,53 @@ export default {
     selectedState: {
       type: String,
       required: true
+    },
+    moduleLabels: {
+      type: Object,
+      required: true
+    },
+    filterValues: {
+      type: Object,
+      required: true
+    },
+    checkedIds: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    getItemsNumber () {
+      return `${this.checkedIds.length} ${this.checkedIds.length === 1
+        ? this.moduleLabels.singular
+        : this.moduleLabels.plural}`;
     }
   }
 };
 </script>
 <style lang='scss' scoped>
-  .collapse-enter-active, .collapse-leave-active {
-    transition: opacity 0.5s;
-  }
-  .collapse-enter-active {
-    opacity: 1;
-  }
-  .collapse-enter, .collapse-leave-to {
-    opacity: 0;
-  }
-
   .apos-select-box {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    box-sizing: border-box;
     overflow: hidden;
-    color: var(--a-text-primary);
-    background-color: var(--a-base-9);
+    height: 5rem;
+    transition: all 0.3s linear;
+
+    &.collapse-enter, &.collapse-leave-to {
+      height: 0;
+    }
+
+    &__content {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: var(--a-base-9);
+      margin-top: 1rem;
+      color: var(--a-text-primary);
+    }
+
+    &__text {
+      @include type-large;
+      text-transform: lowercase;
+    }
   }
 
 </style>
