7bfd35aa6c1cd955402c190e566d2eb164a15274
Jed <valentin.jeudy@gmail.com>
2021-11-08 13:17:15 +0100

    created AposFile component

---
+++ modules/@apostrophecms/ui/ui/apos/components/AposFile.vue
@@ -0,0 +1,159 @@
+<template>
+  <div>
+    <label
+      class="apos-input-wrapper apos-file-dropzone"
+      :class="{
+        'apos-file-dropzone--dragover': dragging,
+        'apos-is-disabled': disabled
+      }"
+      @drop.prevent="uploadFile"
+      @dragover="dragHandler"
+      @dragleave="dragging = false"
+    >
+      <p class="apos-file-instructions">
+        <template v-if="dragging">
+          <cloud-upload-icon :size="38" />
+        </template>
+        <AposSpinner v-else-if="uploading" />
+        <template v-else>
+          <paperclip-icon :size="14" class="apos-file-icon" />
+          {{ messages.primary }}&nbsp;
+          <span class="apos-file-highlight" v-if="messages.highlighted">
+            {{ messages.highlighted }}
+          </span>
+        </template>
+      </p>
+      <input
+        type="file"
+        class="apos-sr-only"
+        :disabled="disabled"
+        @input="uploadFile"
+        :accept="field.accept"
+      >
+    </label>
+    <!-- <div v-if="next && next._id" class="apos-file-files">
+      <AposSlatList
+        :value="next ? [ next ] : []"
+        @input="updated"
+        :disabled="field.readOnly"
+      />
+    </div> -->
+  </div>
+</template>
+
+<script>
+// import AposSlatList from 'Modules/@apostrophecms/ui/components/AposSlatList';
+
+export default {
+  // components: {
+  //   AposSlatList
+  // },
+  props: {
+    uploading: {
+      type: Boolean,
+      default: false
+    },
+    field: {
+      type: Object,
+      default: () => ({})
+    },
+    allowedExtensions: {
+      type: Array,
+      default: () => [ '*' ]
+    }
+  },
+  emits: [ 'upload-file' ],
+  data: () => ({
+    next: (this.value && (typeof this.value.data === 'object'))
+      ? this.value.data : (this.field.def || null),
+    disabled: false,
+    dragging: false
+  }),
+  computed: {
+    messages () {
+      const msgs = {
+        primary: 'Drop a file here or',
+        highlighted: 'click to open the file explorer'
+      };
+      if (this.disabled) {
+        msgs.primary = 'Field is disabled';
+        msgs.highlighted = '';
+      }
+      if (this.limitReached) {
+        msgs.primary = 'Attachment limit reached';
+        msgs.highlighted = '';
+      }
+      return msgs;
+    }
+    // limitReached () {
+    //   return !!(this.value.data && this.value.data._id);
+    // }
+  },
+  methods: {
+    uploadFile (e) {
+      this.dragging = false;
+      this.disabled = true;
+
+      this.$emit('upload-file', e);
+    },
+    dragHandler ({ preventDefault }) {
+      preventDefault();
+      if (!this.disabled && !this.dragging) {
+        this.dragging = true;
+      }
+    }
+  }
+};
+</script>
+<style scoped lang='scss'>
+  .apos-file-dropzone {
+    @include apos-button-reset();
+    @include type-base;
+    display: block;
+    margin: 10px 0;
+    padding: 20px;
+    border: 2px dashed var(--a-base-8);
+    border-radius: var(--a-border-radius);
+    transition: all 0.2s ease;
+    &:hover {
+      border-color: var(--a-primary);
+      background-color: var(--a-base-10);
+    }
+    &:active,
+    &:focus {
+      border: 2px solid var(--a-primary);
+    }
+    &.apos-is-disabled {
+      color: var(--a-base-4);
+      background-color: var(--a-base-7);
+      border-color: var(--a-base-4);
+
+      &:hover {
+        cursor: not-allowed;
+      }
+    }
+  }
+
+  .apos-file-dropzone--dragover {
+    border: 2px dashed var(--a-primary);
+    background-color: var(--a-base-10);
+  }
+
+  .apos-file-instructions {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    pointer-events: none;
+    // v-html goofiness
+    & ::v-deep .apos-file-highlight {
+      color: var(--a-primary);
+      font-weight: var(--a-weight-bold);
+    }
+  }
+
+  .apos-file-icon {
+    transform: rotate(45deg);
+    margin-right: 5px;
+  }
+</style>
