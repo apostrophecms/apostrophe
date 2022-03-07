39947c2aae50e783ca8eddfc2882eae81d29d5a0
Thomas Boutell <tom@apostrophecms.com>
2022-03-07 08:37:56 -0500

    wip, very wip

--- modules/@apostrophecms/asset/lib/webpack/apos/vue-2-loader.js
+++ modules/@apostrophecms/asset/lib/webpack/apos/vue-2-loader.js
@@ -2,21 +2,40 @@
 // const utils = require('@vue/component-compiler-utils');
 const parser = require('node-html-parser');
 const compiler = require('vue-template-compiler');
+// Without this the render functions contain with(), which breaks
+// strict mode. That's why vue-loader also uses it
+const strictifier = require('vue-template-es2015-compiler');
 const sass = require('sass');
 
 module.exports = function(source) {
-
-  let parsed;
-  try {
-    parsed = utils.parse({
-      source,
-      compiler
-    });
-    console.log('OOOOO:', !!parsed.template, !!parsed.script);
-  } catch (e) {
-    console.error('XXXXX', e);
-    return '';
+  const parsed = parser.parse(source);
+  let child = parsed.firstChild;
+  let output = '';
+  // console.log(this._module);
+  while (child) {
+    if (child.tagName === 'TEMPLATE') {
+      console.log('>> template');
+      const rawCode = compiler.compile(child.innerHTML).render;
+      console.log(rawCode);
+      output += strictifier(
+        `var render = function (h) {${rawCode}};`
+      );
+    } else if (child.tagName === 'SCRIPT') {
+      console.log('>> script');
+      output += child.textContent;
+    } else if (child.tagName === 'STYLE') {
+      console.log('>> style');
+      output += sassLoader(child.textContent);
+    } else if (child.tagName) {
+      console.log(`==> ${child.tagName}`);
+    } else {
+      console.log(child);
+    }
+    child = child.nextElementSibling;
+    console.log(!!child);
   }
+  // console.log('>>>', output);
+  return output;
 
   return '';
   // const templateContent = parsed.template.content;
