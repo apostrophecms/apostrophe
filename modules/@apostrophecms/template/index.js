3c26a90e4076a4d2d9b5db1a67a6ca0cc75975e9
Jed <vjeudy@protonmail.com>
2022-03-22 17:58:05 +0100

    adds methods to fill page template with needed bundles at page load time

--- modules/@apostrophecms/template/index.js
+++ modules/@apostrophecms/template/index.js
@@ -630,8 +630,6 @@ module.exports = {
       // async function.
 
       async renderPageForModule(req, template, data, module) {
-
-        let content;
         let scene = req.user ? 'apos' : 'public';
         if (req.scene) {
           scene = req.scene;
@@ -698,14 +696,24 @@ module.exports = {
         }
 
         try {
-          content = await module.render(req, template, args);
+          const content = await module.render(req, template, args);
+
+          const filledContent = addExtraBundles({
+            page: req.data.bestPage,
+            scene,
+            template,
+            content,
+            scriptsPlaceholder: req.scriptsPlaceholder,
+            stylesheetsPlaceholder: req.stylesheetsPlaceholder,
+            widgetsBundles: req.widgetsBundles
+          });
+
+          return filledContent;
         } catch (e) {
           // The page template threw an exception. Log where it
           // occurred for easier debugging
           return error(e);
         }
-
-        return content;
 
         function error(e) {
           self.logError(req, e);
@@ -727,6 +735,157 @@ module.exports = {
           } else {
             return url.replace('&aposRefresh=1', '');
           }
+        }
+
+        function addExtraBundles ({
+          page = {},
+          template = '',
+          scene,
+          content,
+          scriptsPlaceholder,
+          stylesheetsPlaceholder,
+          widgetsBundles = {}
+        }) {
+          if (!scriptsPlaceholder && !stylesheetsPlaceholder) {
+            return content;
+          }
+
+          const { es5 } = self.apos.asset.options;
+          const { extraBundles } = self.apos.asset;
+          const renderMarkup = renderBundleMarkup(self.apos.asset.getAssetBaseUrl());
+          const jsMainBundle = renderMarkup({
+            fileName: `${scene}-module-bundle`,
+            fileNameNoMod: `${scene}-nomodule-bundle`,
+            ext: 'js',
+            es5
+          });
+          const cssMainBundle = renderMarkup({
+            fileName: `${scene}-bundle`,
+            ext: 'css'
+          });
+
+          if (scene === 'apos') {
+            return loadAllBundles({
+              content,
+              scriptsPlaceholder,
+              stylesheetsPlaceholder,
+              extraBundles,
+              renderMarkup,
+              jsMainBundle,
+              cssMainBundle,
+              es5
+            });
+          }
+
+          const templateType = template.substring(template.lastIndexOf(':') + 1);
+          const { webpack } = page.type ? self.apos.modules[page.type].__meta : {};
+
+          const configs = Object.values(webpack || {}).reduce((acc, config) => ({
+            ...acc,
+            ...config && config.bundles
+          }), widgetsBundles);
+
+          const { jsBundles, cssBundles } = Object.entries(configs)
+            .reduce((acc, [ name, { templates } ]) => {
+              if (templates && !templates.includes(templateType)) {
+                return acc;
+              }
+
+              const jsMarkup = scriptsPlaceholder &&
+              extraBundles.js.includes(`${name}.js`) &&
+                renderMarkup({
+                  fileName: name,
+                  ext: 'js'
+                });
+
+              const cssMarkup = stylesheetsPlaceholder &&
+                extraBundles.css.includes(`${name}.css`) &&
+                renderMarkup({
+                  fileName: name,
+                  ext: 'css'
+                });
+
+              return {
+                jsBundles: stripIndent`
+                  ${acc.jsBundles}
+                  ${jsMarkup || ''}
+                `,
+                cssBundles: stripIndent`
+                  ${acc.cssBundles}
+                  ${cssMarkup || ''}
+                `
+              };
+            }, {
+              jsBundles: jsMainBundle,
+              cssBundles: cssMainBundle
+            });
+
+          return content
+            .replace(scriptsPlaceholder, jsBundles)
+            .replace(stylesheetsPlaceholder, cssBundles);
+        }
+
+        function loadAllBundles({
+          content,
+          extraBundles,
+          scriptsPlaceholder,
+          stylesheetsPlaceholder,
+          jsMainBundle,
+          cssMainBundle,
+          renderMarkup
+        }) {
+          const jsBundles = extraBundles.js.reduce((acc, bundle) => {
+            const bundleMarkup = renderMarkup({
+              fileName: bundle.replace('.js', ''),
+              ext: 'js'
+            });
+
+            return stripIndent`
+              ${acc}
+              ${bundleMarkup}
+            `;
+
+          }, jsMainBundle);
+
+          const cssBundles = extraBundles.css.reduce((acc, bundle) => {
+            const bundleMarkup = renderMarkup({
+              fileName: bundle.replace('.css', ''),
+              ext: 'css'
+            });
+
+            return stripIndent`
+              ${acc}
+              ${bundleMarkup}
+            `;
+
+          }, cssMainBundle);
+
+          return content
+            .replace(scriptsPlaceholder, jsBundles)
+            .replace(stylesheetsPlaceholder, cssBundles);
+        }
+
+        function renderBundleMarkup (base) {
+          return ({
+            fileName, fileNameNoMod, ext = 'js', es5 = false
+          }) => {
+            if (ext === 'css') {
+              return self.apos.template.safe(stripIndent`
+                <link href="${base}/${fileName}.css" rel="stylesheet" />
+              `);
+            }
+
+            if (es5) {
+              return self.apos.template.safe(stripIndent`
+                <script nomodule src="${base}/${fileNameNoMod}.${ext}"></script>
+                <script type="module" src="${base}/${fileName}.${ext}"></script>
+              `);
+            }
+
+            return self.apos.template.safe(stripIndent`
+              <script src="${base}/${fileName}.${ext}"></script>
+            `);
+          };
         }
       },
 
