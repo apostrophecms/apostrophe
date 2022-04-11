2b3a8723ca2921b433e5861d251bbaf9fab2c213
Jed <vjeudy@protonmail.com>
2022-04-11 19:37:33 +0200

    modernizes and move debounce function to utils

---
+++ modules/@apostrophecms/ui/ui/apos/utils/index.js
@@ -0,0 +1,9 @@
+export const debounce = (func, timeout) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+};
