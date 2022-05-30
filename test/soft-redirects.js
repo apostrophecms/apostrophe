eed7512842abd0730498d448e9e4e0fdc4394c4a
Etienne Laurent <etlaurent@tuta.io>
2022-05-30 18:41:37 +0300

    keep test

--- test/soft-redirects.js
+++ test/soft-redirects.js
@@ -74,6 +74,20 @@ describe('Soft Redirects', function() {
 
   it('should be able to serve the page at its old URL too, via redirect', function(done) {
     return request({
+      url: 'http://localhost:7900/child',
+      followRedirect: false
+    }, function(err, response, body) {
+      assert(!err);
+      // Is our status code good?
+      assert.equal(response.statusCode, 302);
+      // Are we going to be redirected to our page?
+      assert.equal(response.headers.location, '/child-moved');
+      return done();
+    });
+  });
+
+  it('should be able to serve the page at its old URL too, via redirect, with query params', function(done) {
+    return request({
       url: 'http://localhost:7900/child?a-query-param=foo&another',
       followRedirect: false
     }, function(err, response, body) {
@@ -150,6 +164,20 @@ describe('Soft Redirects - with `statusCode` option', function() {
   });
 
   it('should be able to serve the page at its old URL too, via redirect', function(done) {
+    return request({
+      url: 'http://localhost:7900/child',
+      followRedirect: false
+    }, function(err, response, body) {
+      assert(!err);
+      // Is our status code good?
+      assert.equal(response.statusCode, 301);
+      // Are we going to be redirected to our page?
+      assert.equal(response.headers.location, '/child-moved');
+      return done();
+    });
+  });
+
+  it('should be able to serve the page at its old URL too, via redirect, with query params', function(done) {
     return request({
       url: 'http://localhost:7900/child?a-query-param=foo&another',
       followRedirect: false
