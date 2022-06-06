c64834073b262463458945b88e12ccdab719b50a
Etienne Laurent <etlaurent@tuta.io>
2022-06-06 12:19:37 +0200

    implement modal display wip

---
+++ modules/@apostrophecms/modal/ui/apos/components/AposModalShareDraft.vue
@@ -0,0 +1,167 @@
+<template>
+  <AposModal
+    :modal="modal"
+    class="apos-share-draft"
+    v-on="{ esc: close }"
+    @no-modal="$emit('safe-close')"
+    @inactive="modal.active = false"
+    @show-modal="modal.showModal = true"
+  >
+    <template #main>
+      <AposModalBody>
+        <template #bodyMain>
+          <div class="apos-share-draft__header">
+            <h2 class="apos-share-draft__heading">
+              {{ $t('apostrophe:shareDraftHeader') }}
+            </h2>
+            <a
+              href=""
+              class="apos-share-draft__close"
+              @click.prevent="close"
+            >
+              ×
+            </a>
+          </div>
+          <div class="apos-share-draft__content">
+            <div class="apos-share-draft__toggle-wrapper">
+              <AposToggle
+                v-model="disabled"
+                class="apos-share-draft__toggle"
+                @toggle="toggle"
+              />
+              <p class="apos-share-draft__toggle-label">
+                {{ $t('apostrophe:shareDraftEnable') }}
+              </p>
+            </div>
+            <p class="apos-share-draft__description">
+              {{ $t('apostrophe:shareDraftDescription') }}
+            </p>
+            <input
+              type="text"
+              value="https://dummy-url/wefe3456wfewfki9"
+              disabled
+              class="apos-share-draft__url"
+            >
+            <a
+              href=""
+              class="apos-share-draft__copy-link"
+              @click.prevent="copy"
+            >
+              {{ $t('apostrophe:shareDraftCopyLink') }}
+            </a>
+          </div>
+        </template>
+      </AposModalBody>
+    </template>
+  </AposModal>
+</template>
+
+<script>
+export default {
+  emits: [ 'safe-close' ],
+  data() {
+    return {
+      modal: {
+        active: false,
+        type: 'overlay',
+        showModal: false,
+        disableHeader: true,
+        trapFocus: true
+      },
+      disabled: false
+    };
+  },
+  async mounted() {
+    this.modal.active = true;
+  },
+  methods: {
+    copy() {
+      console.log('copy');
+    },
+    toggle() {
+      console.log('toggle');
+    },
+    close() {
+      console.log('close');
+      this.modal.showModal = false;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.apos-share-draft {
+  z-index: $z-index-modal;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+::v-deep .apos-modal__inner {
+  top: auto;
+  right: auto;
+  bottom: auto;
+  left: auto;
+  max-width: 700px;
+  height: auto;
+  text-align: center;
+}
+
+::v-deep .apos-modal__overlay {
+  .apos-modal + .apos-share-draft & {
+    display: block;
+  }
+}
+
+::v-deep .apos-modal__body {
+  padding: 20px;
+}
+
+::v-deep .apos-modal__body-main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.apos-share-draft__header {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  border-bottom: 1px solid var(--a-base-8);
+  padding: 0 20px 20px;
+}
+
+.apos-share-draft__heading {
+  @include type-title;
+  line-height: var(--a-line-tall);
+  margin: 0;
+}
+
+.apos-share-draft__close {
+  text-decoration: none;
+  align-self: center;
+}
+
+.apos-share-draft__content {
+  margin-top: $spacing-double;
+}
+
+.apos-share-draft__description {
+  @include type-small;
+  margin-top: $spacing-double;
+  line-height: var(--a-line-tall);
+  max-width: 355px;
+  color: var(--a-base-2);
+}
+
+// .apos-share-draft__description {
+//   @include type-base;
+//   max-width: 370px;
+//   line-height: var(--a-line-tallest);
+// }
+</style>
