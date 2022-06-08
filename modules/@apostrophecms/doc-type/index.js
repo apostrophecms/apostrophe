3cd42905c62961b04974580023f587b3f9cc0b67
Harouna Traoré <haroun@users.noreply.github.com>
2022-06-08 18:40:46 +0200

    implement share/unshare

--- modules/@apostrophecms/doc-type/index.js
+++ modules/@apostrophecms/doc-type/index.js
@@ -1202,11 +1202,51 @@ module.exports = {
       },
 
       async share(req, doc) {
-        return doc;
+        if (doc._edit !== true) {
+          throw self.apos.error('notfound');
+        }
+
+        if (!doc._url) {
+          return doc;
+        }
+
+        const { aposShareKey: _aposShareKey, ...draft } = doc;
+        const aposShareKey = doc.aposShareKey || self.apos.util.generateId();
+
+        await self.apos.doc.db.updateOne({
+          _id: doc._id
+        }, {
+          $set: {
+            aposShareKey
+          }
+        });
+
+        return {
+          ...draft,
+          aposShareKey
+        };
       },
 
       async unshare(req, doc) {
-        return doc;
+        if (doc._edit !== true) {
+          throw self.apos.error('notfound');
+        }
+
+        if (!doc._url) {
+          return doc;
+        }
+
+        const { aposShareKey: _aposShareKey, ...draft } = doc;
+
+        await self.apos.doc.db.updateOne({
+          _id: doc._id
+        }, {
+          $unset: {
+            aposShareKey: 1
+          }
+        });
+
+        return draft;
       }
     };
   },
