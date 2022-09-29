6f101c08f63716b7f30f29bacc7561ce90819fc0
ETLaurent <ETLaurent@users.noreply.github.com>
2022-09-29 11:14:17 +0200

    display placeholder for image-widget (#3884)

--- test/modules/placeholder-page/index.js
+++ test/modules/placeholder-page/index.js
@@ -10,7 +10,8 @@ module.exports = {
         label: 'Main',
         options: {
           widgets: {
-            placeholder: {}
+            placeholder: {},
+            '@apostrophecms/image': {}
           }
         }
       }
