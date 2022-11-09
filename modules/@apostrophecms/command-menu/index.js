2420ba8d39cdc8630e3f5f2fab3effac327138fe
Harouna Traoré <haroun@users.noreply.github.com>
2022-11-09 17:07:09 +0100

    validate commands and groups

--- modules/@apostrophecms/command-menu/index.js
+++ modules/@apostrophecms/command-menu/index.js
@@ -1,3 +1,4 @@
+const assert = require('assert').strict;
 const pipe = (...functions) => (initial) => functions.reduce((accumulator, current) => current(accumulator), initial);
 
 module.exports = {
@@ -9,16 +10,48 @@ module.exports = {
       'apostrophe:ready': {
         composeCommands() {
           self.rawCommands = Object.values(self.apos.modules).flatMap(self.composeCommandsForModule);
+          self.commands = {};
+          self.groups = {};
 
           const composed = pipe(self.composeCommand, self.composeRemove, self.composeGroup)({ rawCommands: self.rawCommands });
-          self.commands = composed.command;
-          self.groups = composed.group;
+          try {
+            Object.entries(composed.command).every(([ name, command ]) => self.validateCommand({ name, command }));
+            Object.entries(composed.group).every(([ name, group ]) => self.validateGroup({ name, group }));
+
+            self.commands = composed.command;
+            self.groups = composed.group;
+          } catch (error) {
+            self.apos.util.error('Command-Menu', error.message);
+          }
         }
       }
     };
   },
   methods(self) {
     return {
+      validateCommand({ name, command }) {
+        assert.equal(command.type, 'item', `Invalid command type, must be "item", for ${name}`);
+        assert.equal(typeof command.label, 'string', `Invalid command label, must be a string, for ${name} "${typeof command.label}" provided`);
+        assert.equal(typeof command.action, 'object', `Invalid command action, must be an object for ${name}`);
+        assert.equal(typeof command.action.type, 'string', `Invalid command action type for ${name}`);
+        assert.equal(typeof command.action.payload, 'object', `Invalid command action payload for ${name}`);
+        if (command.permission) {
+          assert.equal(typeof command.permission, 'object', `Invalid command permission for ${name}`);
+          assert.equal(typeof command.permission.action, 'string', `Invalid command permission action for ${name}`);
+          assert.equal(typeof command.permission.type, 'string', `Invalid command permission type for ${name}`);
+        }
+        assert.equal(typeof command.modal, 'string', `Invalid command modal for ${name}`);
+        assert.equal(typeof command.shortcut, 'string', `Invalid command shortcut, must be a string, for ${name}`);
+
+        return true;
+      },
+      validateGroup({ name, group }) {
+        assert.equal(typeof group.label, 'string', `Invalid group label, must be a string, for ${name}`);
+        assert.equal(Array.isArray(group.fields), true, `Invalid command fields, must be an array for ${name}`);
+        assert.ok(group.fields.every(field => typeof field === 'string'), `Invalid command fields, must contains strings, for ${name}`);
+
+        return true;
+      },
       composeRemove(initialState) {
         const concatenate = []
           .concat(...initialState.rawCommands.map(command => command.remove))
