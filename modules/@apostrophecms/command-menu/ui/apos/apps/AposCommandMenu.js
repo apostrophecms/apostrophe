77ccde4cdbb554a3dc0dcf20cda5ff9e0c6f373d
Harouna Traoré <haroun@users.noreply.github.com>
2022-12-21 09:41:25 +0100

    check apos.commandMenu exists before using it

--- modules/@apostrophecms/command-menu/ui/apos/apps/AposCommandMenu.js
+++ modules/@apostrophecms/command-menu/ui/apos/apps/AposCommandMenu.js
@@ -1,31 +1,34 @@
 import Vue from 'Modules/@apostrophecms/ui/lib/vue';
 
 export default function() {
-  const theAposCommandMenu = new Vue({
-    el: '#apos-command-menu',
-    computed: {
-      apos () {
-        return window.apos;
-      }
-    },
-    methods: {
-      getModal() {
-        return this.$refs.commandMenu.modal;
-      }
-    },
-    render(h) {
-      return h(
-        apos.commandMenu.components.the,
-        {
-          ref: 'commandMenu',
-          props: {
-            groups: apos.commandMenu.groups,
-            modals: apos.commandMenu.modals
-          }
+  // Careful, login page is in user scene but has command menu
+  if (apos.commandMenu) {
+    const theAposCommandMenu = new Vue({
+      el: '#apos-command-menu',
+      computed: {
+        apos () {
+          return window.apos;
         }
-      );
-    }
-  });
+      },
+      methods: {
+        getModal() {
+          return this.$refs.commandMenu.modal;
+        }
+      },
+      render(h) {
+        return h(
+          apos.commandMenu.components.the,
+          {
+            ref: 'commandMenu',
+            props: {
+              groups: apos.commandMenu.groups,
+              modals: apos.commandMenu.modals
+            }
+          }
+        );
+      }
+    });
 
-  apos.commandMenu.getModal = theAposCommandMenu.getModal;
+    apos.commandMenu.getModal = theAposCommandMenu.getModal;
+  }
 }
