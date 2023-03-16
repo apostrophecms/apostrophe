db8b9f8cb288704d4bc86e506e4b209180e36d78
Anthony Tarlao <anthony.tarlao@gmail.com>
2023-03-16 11:17:26 +0100

    Login header based on env variable enhancement

--- modules/@apostrophecms/login/index.js
+++ modules/@apostrophecms/login/index.js
@@ -382,8 +382,18 @@ module.exports = {
             }
           }
         }
+
+        let labelEnv;
+        if (self.apos.options.multisite) {
+          const envNames = self.apos.modules.site ? self.apos.modules.site.envNames : self.apos.options.multisite.envNames;
+          const activeEnv = self.apos.modules.site ? self.apos.modules.site.activeEnv : self.apos.options.multisite.activeEnv;
+          labelEnv = activeEnv === envNames[0]
+            ? 'development' : activeEnv === 'prod'
+              ? 'production' : 'staging';
+        }
+
         return {
-          env: process.env.NODE_ENV || 'development',
+          env: process.env.ENV_LABEL || labelEnv || process.env.NODE_ENV || 'development',
           name: (process.env.npm_package_name && process.env.npm_package_name.replace(/-/g, ' ')) || 'Apostrophe',
           version: aposPackage.version || '3',
           requirementProps
--- modules/@apostrophecms/login/ui/apos/components/TheAposLoginHeader.vue
+++ modules/@apostrophecms/login/ui/apos/components/TheAposLoginHeader.vue
@@ -110,7 +110,7 @@ export default {
       background: var(--a-danger);
     }
 
-    &--success {
+    &--success, &--staging {
       background: var(--a-warning);
     }
   }
