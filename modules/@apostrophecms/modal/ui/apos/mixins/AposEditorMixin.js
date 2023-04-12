c23a90ebe486c2a3feba9c459072638d73eb5d57
Etienne Laurent <etldev@tuta.io>
2023-04-12 11:35:10 +0200

    add comment

--- modules/@apostrophecms/modal/ui/apos/mixins/AposEditorMixin.js
+++ modules/@apostrophecms/modal/ui/apos/mixins/AposEditorMixin.js
@@ -253,6 +253,10 @@ export default {
             break;
           }
 
+          // Use the default values defined in the schema fields
+          // to fix conditional fields not showing up when editing existing docs,
+          // in the case where new fields have been added to the schema
+          // but not (yet) saved to the document:
           const fieldValue = self.getFieldValue(key);
           const fieldValueOrDef = fieldValue !== undefined
             ? fieldValue
