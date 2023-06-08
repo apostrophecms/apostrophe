af528192a0b1cb4c8870fca12972ece3d61cf6f4
Etienne Laurent <etldev@tuta.io>
2023-06-08 15:22:59 -0400

    addd the possibility to place bars below others

--- modules/@apostrophecms/admin-bar/index.js
+++ modules/@apostrophecms/admin-bar/index.js
@@ -355,8 +355,30 @@ module.exports = {
         };
       },
 
+      // Add custom bars and place the ones
+      // that have `last: true` at the end
+      // of the list so that they will be
+      // displayed below the others.
+      //
+      // Example:
+      //
+      // ```js
+      // self.addBar({
+      //   id: 'template',
+      //   componentName: 'TheAposTemplateBar',
+      //   props: { content: 'Some content' },
+      //   last: true
+      // });
+      // ```
       addBar(bar) {
         self.bars.push(bar);
+
+        self.bars.sort((a, b) => {
+          if (a.last === true && b.last === true) {
+            return 0;
+          }
+          return b.last === true ? -1 : 1;
+        });
       }
     };
   }
