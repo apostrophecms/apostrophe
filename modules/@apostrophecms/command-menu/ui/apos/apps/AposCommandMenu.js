a958d744f112eb9e3f87eaa743af92a00f4f74b5
Jed <vjeudy@protonmail.com>
2023-12-04 11:39:07 +0100

    passes right props to TheAposCommandMenu, moves function from data to methods

--- modules/@apostrophecms/command-menu/ui/apos/apps/AposCommandMenu.js
+++ modules/@apostrophecms/command-menu/ui/apos/apps/AposCommandMenu.js
@@ -4,7 +4,9 @@ import TheAposCommandMenu from '../components/TheAposCommandMenu.vue';
 export default function() {
   // Careful, login page is in user scene but has no command menu
   if (apos.commandMenu) {
-    const app = createApp(TheAposCommandMenu);
+    const app = createApp(TheAposCommandMenu, {
+      modals: apos.commandMenu.modals
+    });
     const theAposCommandMenu = app.mount('#apos-command-menu');
 
     apos.commandMenu.getModal = theAposCommandMenu.getModal;
