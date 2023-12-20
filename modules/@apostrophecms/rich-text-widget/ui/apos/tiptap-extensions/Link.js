52ae563ac4071ce52b265eb11a6458b39835044e
Thomas Boutell <tom@apostrophecms.com>
2023-12-20 16:14:04 -0500

    finesse

--- modules/@apostrophecms/rich-text-widget/ui/apos/tiptap-extensions/Link.js
+++ modules/@apostrophecms/rich-text-widget/ui/apos/tiptap-extensions/Link.js
@@ -6,7 +6,18 @@ export default (options) => {
         ...this.parent?.(),
         openOnClick: false,
         linkOnPaste: true,
+        // Intentional, the default-defaults are poorly suited
+        // to CMS work e.g. nofollow
         HTMLAttributes: {}
+      };
+    },
+    addAttributes() {
+      return {
+        // Necessary to not break href, etc.
+        ...this.parent?.(),
+        'aria-label': {
+          default: null
+        }
       };
     }
   });
