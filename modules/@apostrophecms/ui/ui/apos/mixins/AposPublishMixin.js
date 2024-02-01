ac93e411594ef8bba4772b774f9a745911957058
Anthony Tarlao <anthony.tarlao@gmail.com>
2024-02-01 14:21:39 +0100

    Display a more informative error message when publishing a page

--- modules/@apostrophecms/ui/ui/apos/mixins/AposPublishMixin.js
+++ modules/@apostrophecms/ui/ui/apos/mixins/AposPublishMixin.js
@@ -65,9 +65,10 @@ export default {
               // Retry now that ancestors are published
               return this.publish(doc);
             } catch (e) {
+              const errorMessage = e.message === 'forbidden' ? 'apostrophe:errorWhilePublishingParentPageForbidden' : e.message;
               await apos.alert({
                 heading: this.$t('apostrophe:errorWhilePublishing'),
-                description: e.message || this.$t('apostrophe:errorWhilePublishingParentPage'),
+                description: errorMessage || this.$t('apostrophe:errorWhilePublishingParentPage'),
                 localize: false
               });
             }
