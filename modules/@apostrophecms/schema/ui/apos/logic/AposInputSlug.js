1b5ef25b40016d725c2bec4441354f21bdcccc33
Anthony Tarlao <anthony.tarlao@gmail.com>
2024-02-05 11:01:48 +0100

    Block typing slash key in slugs

--- modules/@apostrophecms/schema/ui/apos/logic/AposInputSlug.js
+++ modules/@apostrophecms/schema/ui/apos/logic/AposInputSlug.js
@@ -276,6 +276,11 @@ export default {
     },
     emitReturn() {
       this.$emit('return');
+    },
+    blockSlash(evt) {
+      if (evt.key === '/') {
+        evt.preventDefault();
+      }
     }
   }
 };
