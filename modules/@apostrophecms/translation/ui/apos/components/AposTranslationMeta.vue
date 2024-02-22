c4a20dd4980615a9536c6b16dfab8520513cc14c
Jed <vjeudy@protonmail.com>
2024-02-22 10:36:15 +0100

    adds new AposTranslationMeta component

---
+++ modules/@apostrophecms/translation/ui/apos/components/AposTranslationMeta.vue
@@ -0,0 +1,37 @@
+<template>
+  <AposTranslationIndicator
+    v-if="isShowed"
+    label="apostrophe:automaticTranslationFieldMeta"
+  />
+</template>
+
+<script>
+export default {
+  name: 'AposTranslationMeta',
+  props: {
+    meta: {
+      type: Object,
+      required: true
+    },
+    metaRaw: {
+      type: Object,
+      required: true
+    },
+    field: {
+      type: Object,
+      required: true
+    },
+    items: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      isShowed: this.meta.data?.changed
+    };
+  }
+};
+</script>
