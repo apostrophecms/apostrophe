6d6474424d9e3dc8b36dc498d68b8bb8547cf300
Anthony Tarlao <anthony.tarlao@gmail.com>
2024-03-06 16:59:43 +0100

    Improve with variables

--- modules/@apostrophecms/schema/ui/apos/components/AposSchema.vue
+++ modules/@apostrophecms/schema/ui/apos/components/AposSchema.vue
@@ -40,7 +40,7 @@
       <component
         v-show="displayComponent(field)"
         v-model="fieldState[field.name]"
-        :class="{ 'apos-field__wrapper--highlight': meta[field.name]?.['@apostrophecms/schema:highlight'] }"
+        :class="{ 'apos-field__wrapper--highlight': highlight(field.name) }"
         :is="fieldComponentMap[field.type]"
         :following-values="followingValues[field.name]"
         :condition-met="conditionalFields?.if[field.name]"
@@ -60,7 +60,7 @@
         v-if="hasCompareMeta"
         v-show="displayComponent(field)"
         v-model="compareMetaState[field.name]"
-        :class="{ 'apos-field__wrapper--highlight': meta[field.name]?.['@apostrophecms/schema:highlight'] }"
+        :class="{ 'apos-field__wrapper--highlight': highlight(field.name) }"
         :is="fieldComponentMap[field.type]"
         :following-values="followingValues[field.name]"
         :condition-met="conditionalFields?.if[field.name]"
@@ -144,6 +144,6 @@ export default {
 
   :deep(.apos-field__wrapper--highlight > .apos-field) {
     padding: 10px;
-    background: #FFF6E8;
+    background: var(--a--highlight);
   }
 </style>
