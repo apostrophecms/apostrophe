2c7e09ccfd8a3e7cd127d4c0fe10d42c355070e3
Jed <vjeudy@protonmail.com>
2024-04-23 18:24:54 +0200

    uses commonjs to export debounce and throttle

--- modules/@apostrophecms/ui/ui/apos/utils/index.js
+++ modules/@apostrophecms/ui/ui/apos/utils/index.js
@@ -1,25 +1,29 @@
-export const debounce = (fn, delay) => {
-  let timer;
-  return (...args) => {
-    return new Promise((resolve) => {
-      clearTimeout(timer);
-      timer = setTimeout(() => {
-        resolve(fn.apply(this, args));
-      }, delay);
-    });
-  };
-};
+module.exports = {
+  debounce(fn, delay) {
+    let timer;
+    return (...args) => {
+      return new Promise((resolve) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+          resolve(fn.apply(this, args));
+        }, delay);
+      });
+    };
+  },
 
-export const throttle = (fn, delay) => {
-  let inThrottle;
+  throttle(fn, delay) {
+    let inThrottle;
 
-  return (...args) => {
-    if (!inThrottle) {
-      inThrottle = true;
-      setTimeout(() => {
-        inThrottle = false;
-        fn.apply(this, args);
-      }, delay);
-    }
-  };
+    return (...args) => {
+      return new Promise((resolve) => {
+        if (!inThrottle) {
+          inThrottle = true;
+          setTimeout(() => {
+            inThrottle = false;
+            resolve(fn.apply(this, args));
+          }, delay);
+        }
+      });
+    };
+  }
 };
