94ff1aaa88e47179113475ef8caa1313ce1f8377
Jed <vjeudy@protonmail.com>
2024-04-23 11:05:38 +0200

    removes useless trycatch from debounce function

--- modules/@apostrophecms/ui/ui/apos/utils/index.js
+++ modules/@apostrophecms/ui/ui/apos/utils/index.js
@@ -1,14 +1,10 @@
 export const debounce = (func, timeout) => {
   let timer;
   return (...args) => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       clearTimeout(timer);
       timer = setTimeout(() => {
-        try {
-          resolve(func.apply(this, args));
-        } catch (err) {
-          reject(err);
-        }
+        resolve(func.apply(this, args));
       }, timeout);
     });
   };
