06e0d3ddffbbdb19d446941fac7512507174c436
Jed <vjeudy@protonmail.com>
2024-07-17 17:58:18 +0200

    uses new locale using label in normal modal too

--- modules/@apostrophecms/ui/ui/apos/components/AposLocale.vue
+++ modules/@apostrophecms/ui/ui/apos/components/AposLocale.vue
@@ -3,17 +3,23 @@
     <span class="apos-locale__label">
       {{ $t('apostrophe:locale') }}:
     </span> <span class="apos-locale__label-name">
-      {{ props.locale }}
+      {{ locale }}
     </span>
   </div>
 </template>
 
 <script setup>
+import { computed } from 'vue';
 const props = defineProps({
   locale: {
     type: String,
     required: true
   }
+});
+
+const locale = computed(() => {
+  const label = apos.i18n.locales[props.locale]?.label;
+  return label ? `${label} (${props.locale})` : props.locale;
 });
 </script>
 
