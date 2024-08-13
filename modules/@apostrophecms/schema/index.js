afc120aaf0727159d59ad321b285a24d3e3b9197
Jed <vjeudy@protonmail.com>
2024-08-13 14:55:40 +0200

    adds default value for options in schema convert method

--- modules/@apostrophecms/schema/index.js
+++ modules/@apostrophecms/schema/index.js
@@ -599,7 +599,7 @@ module.exports = {
       // set error class names, etc. If the error is not a string, it is a
       // database error etc. and should not be displayed in the browser directly.
 
-      async convert(req, schema, data, destination, options) {
+      async convert(req, schema, data, destination, options = { fetchRelationships: true }) {
         if (Array.isArray(req)) {
           throw new Error('convert invoked without a req, do you have one in your context?');
         }
