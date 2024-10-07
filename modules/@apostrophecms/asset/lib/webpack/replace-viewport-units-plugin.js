426e26638e76db7cc5e6eb37c203749ce66b5745
Etienne Laurent <etldev@tuta.io>
2024-10-07 13:55:48 +0200

    remove all existing declarations from the cloned prefixed rule

--- modules/@apostrophecms/asset/lib/webpack/replace-viewport-units-plugin.js
+++ modules/@apostrophecms/asset/lib/webpack/replace-viewport-units-plugin.js
@@ -5,7 +5,7 @@ module.exports = (opts = {}) => {
   // Work with options here
 
   return {
-    postcssPlugin: 'TEST BAGUETTE',
+    postcssPlugin: 'postcss-replace-viewport-units-plugin',
     Root (root, postcss) {
       root.walkRules(rule => {
         const declsWithContainerQueryRelativeUnits = [];
@@ -14,22 +14,24 @@ module.exports = (opts = {}) => {
           if (!decl.value.includes('vh') && !decl.value.includes('vw')) {
             return;
           }
-          const value = decl.value
-            .replaceAll('vh', 'cqh')
-            .replaceAll('vw', 'cqw');
-          console.log('🚀 ~ Root ~ value:', value);
 
-          declsWithContainerQueryRelativeUnits.push(decl.clone({ value }));
+          const declWithContainerQueryRelativeUnits = decl.clone({
+            value: decl.value
+              .replaceAll('vh', 'cqh')
+              .replaceAll('vw', 'cqw')
+          });
+
+          declsWithContainerQueryRelativeUnits.push(declWithContainerQueryRelativeUnits);
         });
 
         if (!declsWithContainerQueryRelativeUnits.length) {
           return;
         }
-        console.dir(declsWithContainerQueryRelativeUnits, { depth: 9 });
 
         const prefixedRule = rule.clone({
           selector: `:where(body[data-breakpoint-preview-mode]) ${rule.selector}`
         });
+        prefixedRule.removeAll();
         prefixedRule.append(...declsWithContainerQueryRelativeUnits);
 
         root.insertAfter(rule, prefixedRule);
