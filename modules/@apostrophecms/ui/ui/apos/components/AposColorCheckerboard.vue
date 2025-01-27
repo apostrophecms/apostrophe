697c0d79c247be1fb38b63bb0aaff328282d4263
Etienne Laurent <git.sabbath464@passmail.net>
2025-01-27 12:48:03 +0100

    use a single shared checkerboard component to fix it in color field and palette

---
+++ modules/@apostrophecms/ui/ui/apos/components/AposColorCheckerboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="apos-color__checkerboard" />
+</template>
+
+<script>
+export default {
+  name: 'AposColorCheckerboard'
+};
+</script>
+
+<style>
+.apos-color__checkerboard {
+  background-image: repeating-conic-gradient(#d3d3d3 0% 25%, #fff 0% 50%);
+  background-size: 8px 8px;
+  height: 100%;
+}
+</style>
