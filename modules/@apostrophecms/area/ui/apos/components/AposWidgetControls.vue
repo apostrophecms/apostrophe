2a5a0edd053eee606fa99a75dc541cc0c38f82f2
Etienne Laurent <git.sabbath464@passmail.net>
2025-04-03 13:31:19 +0100

    display separator based on class

--- modules/@apostrophecms/area/ui/apos/components/AposWidgetControls.vue
+++ modules/@apostrophecms/area/ui/apos/components/AposWidgetControls.vue
@@ -220,9 +220,10 @@ $z-index-button-background: 1;
 $z-index-button-foreground: 2;
 
 .apos-area-modify-controls {
-  :deep(.apos-context-menu__items) .apos-context-menu__item:nth-child(3) {
-    border-bottom: 1px solid var(--a-base-9);
-  }
+  :deep(.apos-context-menu__items)
+    .apos-context-menu__item:has(.apos-context-menu__button--separator) {
+      border-bottom: 1px solid var(--a-base-9);
+    }
 
   :deep(.apos-button__content) {
     z-index: $z-index-button-foreground;
