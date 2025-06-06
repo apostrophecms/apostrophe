4edacd3d24a1dd4b96ef29299f32d584b5be77c3
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2025-06-06 18:48:50 +0300

    PRO-7707: Conditions with following values and mongodb-style comparison operators (#4968)

--- modules/@apostrophecms/schema/ui/apos/mixins/AposInputFollowingMixin.js
+++ modules/@apostrophecms/schema/ui/apos/mixins/AposInputFollowingMixin.js
@@ -7,11 +7,16 @@ export default {
   methods: {
     // Accept a `data` object with field values and return an object
     // of the following values for each field of the underlying schema.
-    computeFollowingValues(data) {
+    computeFollowingValues(data, parentOnly = false) {
       const followingValues = {};
       const parentFollowing = {};
       for (const [ key, val ] of Object.entries(this.followingValues || {})) {
         parentFollowing[`<${key}`] = val;
+      }
+
+      if (parentOnly) {
+        // If we are only interested in the parent following values, return them
+        return parentFollowing;
       }
 
       for (const field of this.schema) {
