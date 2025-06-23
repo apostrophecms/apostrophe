2ce2bd9ae7ad6ead8e463835f33dfb4502fc304e
Jed <vjeudy@protonmail.com>
2025-06-23 17:22:17 +0200

    create AposImageWidget vue component

---
+++ modules/@apostrophecms/image-widget/ui/apos/components/AposImageWidget.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="apos-image-widget">
+    <AposMediaUploaderUi
+      :min-size="[100, 100]"
+      :accept="accept"
+      @upload="upload"
+      @media="media"
+    />
+  </div>
+</template>
+
+<script setup>
+const moduleOptions = window.apos.modules['@apostrophecms/image'];
+const accept = moduleOptions.schema.find(field => field.name === 'attachment').accept;
+
+function media() {
+}
+
+function upload() {
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
