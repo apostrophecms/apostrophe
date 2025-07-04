ad7038991ad9dc42528c22ce41715ee7ff60d814
Jed <vjeudy@protonmail.com>
2025-07-04 15:40:31 +0200

    removes specific draft logic in client http remote method, conserve req.query in getWithQuery middleware

--- modules/@apostrophecms/express/index.js
+++ modules/@apostrophecms/express/index.js
@@ -423,17 +423,13 @@ module.exports = {
       convertPostToGetWithQuery(req, res, next) {
         if (req.method === 'POST' && req.body?.__aposGetWithQuery) {
           req.method = 'GET';
-          req.query = req.body.__aposGetWithQuery;
+          req.query = {
+            ...req.query,
+            ...req.body.__aposGetWithQuery
+          };
           delete req.body;
-          if (!req.url.includes('?')) {
-            req.url = req.url + '?' + qs.stringify(req.query);
-          } else {
-            const [ url, queryString ] = req.url.split('?');
-            const firstPart = queryString.endsWith('&')
-              ? queryString
-              : (queryString ? `${queryString}&` : '');
-            req.url = `${url}?${firstPart}${qs.stringify(req.query)}`;
-          }
+          const [ url ] = req.url.split('?');
+          req.url = `${url}?${qs.stringify(req.query)}`;
         }
 
         return next();
