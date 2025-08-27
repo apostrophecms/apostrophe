a9711a3cc262ee8b531b92e87e7048c294728aff
Etienne Laurent <git.sabbath464@passmail.net>
2025-08-27 15:36:08 +0200

    fix eslint in config file

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,12 @@ const { defineConfig, globalIgnores } = require('eslint/config');
 
 module.exports = defineConfig([
   globalIgnores([
-    "**/vendor/**/*.js",
-    "**/blueimp/**/*.js",
-    "**/node_modules",
-    "test/public",
-    "test/apos-build",
-    "coverage"
+    '**/vendor/**/*.js',
+    '**/blueimp/**/*.js',
+    '**/node_modules',
+    'test/public',
+    'test/apos-build',
+    'coverage'
   ]),
-	apostrophe
+  apostrophe
 ]);
