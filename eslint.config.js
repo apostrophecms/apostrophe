80b3004bf055cae22b2ecdd0d73099eafe730dbb
Etienne Laurent <git.sabbath464@passmail.net>
2025-09-08 14:21:22 +0200

    remove ignored line now included in apostrophe eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,6 @@ module.exports = defineConfig([
   globalIgnores([
     '**/vendor/**/*.js',
     '**/blueimp/**/*.js',
-    '**/node_modules',
     'test/public',
     'test/apos-build',
     'coverage'
