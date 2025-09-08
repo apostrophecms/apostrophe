54b821bcc34f70bcc8e96825c8e5343c56f0968d
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2025-09-08 22:20:08 +0300

    Fix lint

--- modules/@apostrophecms/layout-column-widget/index.js
+++ modules/@apostrophecms/layout-column-widget/index.js
@@ -69,107 +69,109 @@ module.exports = {
       }
     };
   },
-  fields: {
-    add: {
-      desktop: {
-        type: 'object',
-        fields: {
-          add: {
-            colstart: {
-              type: 'integer',
-              required: true
-            },
-            colspan: {
-              type: 'integer',
-              required: true
-            },
-            rowstart: {
-              type: 'integer',
-              def: 1
-            },
-            rowspan: {
-              type: 'integer',
-              def: 1
-            },
-            order: {
-              type: 'integer'
-            },
-            ...alignSchema
+  fields(self, options) {
+    return {
+      add: {
+        desktop: {
+          type: 'object',
+          fields: {
+            add: {
+              colstart: {
+                type: 'integer',
+                required: true
+              },
+              colspan: {
+                type: 'integer',
+                required: true
+              },
+              rowstart: {
+                type: 'integer',
+                def: 1
+              },
+              rowspan: {
+                type: 'integer',
+                def: 1
+              },
+              order: {
+                type: 'integer'
+              },
+              ...alignSchema
+            }
           }
-        }
-      },
-      tablet: {
-        type: 'object',
-        fields: {
-          add: {
-            colstart: {
-              type: 'integer'
-            },
-            colspan: {
-              type: 'integer'
-            },
-            rowstart: {
-              type: 'integer',
-              def: 1
-            },
-            rowspan: {
-              type: 'integer',
-              def: 1
-            },
-            order: {
-              type: 'integer'
-            },
-            show: {
-              type: 'boolean',
-              label: 'Visible on tablet',
-              help: `Less than ${this.options?.breakpoints?.tablet || 900}px`,
-              def: true
-            },
-            ...alignSchema
+        },
+        tablet: {
+          type: 'object',
+          fields: {
+            add: {
+              colstart: {
+                type: 'integer'
+              },
+              colspan: {
+                type: 'integer'
+              },
+              rowstart: {
+                type: 'integer',
+                def: 1
+              },
+              rowspan: {
+                type: 'integer',
+                def: 1
+              },
+              order: {
+                type: 'integer'
+              },
+              show: {
+                type: 'boolean',
+                label: 'apostrophe:layoutTabletShow',
+                help: `Less than ${options?.breakpoints?.tablet || 900}px`,
+                def: true
+              },
+              ...alignSchema
+            }
           }
-        }
-      },
-      mobile: {
-        type: 'object',
-        fields: {
-          add: {
-            colstart: {
-              type: 'integer'
-            },
-            colspan: {
-              type: 'integer'
-            },
-            rowstart: {
-              type: 'integer',
-              def: 1
-            },
-            rowspan: {
-              type: 'integer',
-              def: 1
-            },
-            order: {
-              type: 'integer'
-            },
-            show: {
-              type: 'boolean',
-              label: 'Visible on mobile',
-              help: `Less than ${this.options?.breakpoints?.mobile || 600}px`,
-              def: true
-            },
-            ...alignSchema
+        },
+        mobile: {
+          type: 'object',
+          fields: {
+            add: {
+              colstart: {
+                type: 'integer'
+              },
+              colspan: {
+                type: 'integer'
+              },
+              rowstart: {
+                type: 'integer',
+                def: 1
+              },
+              rowspan: {
+                type: 'integer',
+                def: 1
+              },
+              order: {
+                type: 'integer'
+              },
+              show: {
+                type: 'boolean',
+                label: 'apostrophe:layoutMobileShow',
+                help: `Less than ${options?.breakpoints?.mobile || 600}px`,
+                def: true
+              },
+              ...alignSchema
+            }
           }
-        }
-      },
-      content: {
-        type: 'area',
-        options: {
-          widgets: {
-            '@apostrophecms/rich-text': {},
-            '@apostrophecms/image': {},
-            '@apostrophecms/video': {}
+        },
+        content: {
+          type: 'area',
+          options: {
+            widgets: {
+              '@apostrophecms/rich-text': {},
+              '@apostrophecms/image': {},
+              '@apostrophecms/video': {}
+            }
           }
         }
       }
-    }
+    };
   }
 };
