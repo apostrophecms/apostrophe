89b0b92e8824dd30ce3cface16a2c3bcc6c3582b
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2025-09-11 11:28:52 +0300

    Readonly global exposure

--- modules/@apostrophecms/ui/ui/apos/apps/AposWidgetStore.js
+++ modules/@apostrophecms/ui/ui/apos/apps/AposWidgetStore.js
@@ -3,12 +3,7 @@ import { useWidgetStore } from 'Modules/@apostrophecms/ui/stores/widget.js';
 export default function() {
   const widgetStore = useWidgetStore();
   apos.widget = {
-    refs: widgetStore.refs,
     toId: widgetStore.toId,
     get: widgetStore.get,
-    set: widgetStore.set,
-    getOrSet: widgetStore.getOrSet,
-    update: widgetStore.update,
-    remove: widgetStore.remove
   };
 };
