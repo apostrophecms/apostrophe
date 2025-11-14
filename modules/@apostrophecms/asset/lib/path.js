1b1d209c411373e00b0550e82cc91996ebecbf6d
Tom Boutell <tom@apostrophecms.com>
2025-11-14 17:02:28 -0500

    please eslint

--- modules/@apostrophecms/asset/lib/path.js
+++ modules/@apostrophecms/asset/lib/path.js
@@ -2,7 +2,7 @@ const { glob: globOriginal } = require('glob');
 const {
   resolve: resolveOriginal,
   dirname: dirnameOriginal
- } = require('path');
+} = require('path');
 
 module.exports = {
   glob,
