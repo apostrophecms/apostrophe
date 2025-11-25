ba8cd593cd3325fbe2b3c868712dec9047630297
2400030206 <2400030206@kluniversity.in>
2025-11-26 00:21:26 +0530

    Fix: Async helper function not returning a value in home-page

--- modules/@apostrophecms/home-page/index.js
+++ modules/@apostrophecms/home-page/index.js
@@ -1,3 +1,10 @@
 module.exports = {
-  extend: '@apostrophecms/page-type'
+  extend: '@apostrophecms/page-type',
+  helpers: {
+    async fetchData() {
+      const response = await fetch('https://api.example.com/data');
+      const data = await response.json();
+      return data;
+    }
+  }
 };
