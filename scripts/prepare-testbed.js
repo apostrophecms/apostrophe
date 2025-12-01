2b9e2ab56a4d7ef9215a86c2ecf187db23ca5625
Jed <vjeudy@protonmail.com>
2025-12-01 12:11:32 +0100

    ad prepare-testbed script

---
+++ scripts/prepare-testbed.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+
+const packageJsonFiles = glob.sync('packages/*/package.json');
+
+packageJsonFiles.forEach(file => {
+  const pkg = JSON.parse(fs.readFileSync(file, 'utf8'));
+
+  // Replace workspace:* with actual versions or file: paths
+  ['dependencies', 'devDependencies'].forEach(depType => {
+    if (pkg[depType]) {
+      Object.keys(pkg[depType]).forEach(dep => {
+        if (pkg[depType][dep].startsWith('workspace:')) {
+          // Option A: Use the actual version from the referenced package
+          const depPkgPath = path.join('packages', dep, 'package.json');
+          if (fs.existsSync(depPkgPath)) {
+            const depPkg = JSON.parse(fs.readFileSync(depPkgPath, 'utf8'));
+            pkg[depType][dep] = `^${depPkg.version}`;
+          }
+          // Option B: Or use relative file: paths
+          // pkg[depType][dep] = `file:../${dep}`;
+        }
+      });
+    }
+  });
+
+  fs.writeFileSync(file, JSON.stringify(pkg, null, 2));
+});
