a70ec98a32d01ebcdea95c060b07c3ce70686d80
ETLaurent <git.sabbath464@passmail.net>
2025-12-17 16:35:20 +0100

    add warning

--- packages/apostrophe/modules/@apostrophecms/styles/lib/handlers.js
+++ packages/apostrophe/modules/@apostrophecms/styles/lib/handlers.js
@@ -2,6 +2,25 @@ const { createId } = require('@paralleldrive/cuid2');
 
 module.exports = self => {
   return {
+    'apostrophe:modulesRegistered': {
+      warnDeprecatedPalette() {
+        const paletteModule = self.apos.modules['@apostrophecms-pro/palette'];
+        if (!paletteModule?.tasks?.['migrate-to-styles']) {
+          self.apos.util.warn(
+            `
+                       🎨
+⚠️ @apostrophecms-pro/palette has been deprecated.
+
+Please install the latest version of @apostrophecms-pro/palette and run the following migration task to move your existing palette configuration to
+@apostrophecms/styles: \`node app @apostrophecms-pro/palette:migrate-to-styles\`.
+Then remove @apostrophecms-pro/palette from your project to avoid conflicts.
+                       🎨
+
+`
+          );
+        }
+      }
+    },
     afterSave: {
       async mirrorToGlobal(req, doc, options) {
         // mirror the stylesheet to @apostrophecms/global
