c6b258c81082e045135685afc3fbdc0666b67283
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2025-12-18 14:52:54 +0200

    Fix of style render issues and warnings

--- packages/apostrophe/test/styles.js
+++ packages/apostrophe/test/styles.js
@@ -2627,4 +2627,433 @@ describe('Styles', function () {
       }, 'Nunjucks helpers should render correctly in widget template with inline styles');
     });
   });
+
+  describe('Migrations', function () {
+    let apos;
+
+    before(async function () {
+      apos = await t.create({
+        root: module,
+        modules: {
+          '@apostrophecms/styles': {
+            fields: {
+              add: {
+                textColor: {
+                  type: 'color',
+                  label: 'Text Color',
+                  help: 'Choose a color for the text',
+                  selector: '.palette-playground',
+                  property: 'color'
+                },
+                colorVariable: {
+                  type: 'color',
+                  label: 'Background Color via CSS Variable',
+                  help: 'Used in text background color',
+                  selector: ':root',
+                  property: '--cool-variable-for-palette-testing'
+                },
+                textDecoration: {
+                  type: 'select',
+                  label: 'Text Decoration',
+                  selector: '.palette-playground',
+                  property: 'text-decoration',
+                  choices: [
+                    {
+                      label: 'None',
+                      value: 'none'
+                    },
+                    {
+                      label: 'Underline',
+                      value: 'underline'
+                    }
+                  ]
+                },
+                fontSize: {
+                  label: 'Font Size',
+                  type: 'range',
+                  min: 1,
+                  max: 40,
+                  step: 1,
+                  def: 30,
+                  unit: 'px',
+                  property: 'font-size',
+                  selector: '.palette-playground'
+                },
+                fontFamily: {
+                  label: 'Font Family',
+                  type: 'select',
+                  selector: '.palette-playground',
+                  property: 'font-family',
+                  help: 'Fonts defined in global',
+                  choices: 'getFontFamilyChoices'
+                },
+                borderStyle: {
+                  type: 'select',
+                  label: 'Border Style',
+                  selector: '.palette-playground',
+                  property: 'border-style',
+                  choices: [
+                    {
+                      label: 'None',
+                      value: 'none'
+                    },
+                    {
+                      label: 'Solid',
+                      value: 'solid'
+                    },
+                    {
+                      label: 'Dotted',
+                      value: 'dotted'
+                    }
+                  ]
+                },
+                borderWidth: {
+                  label: 'Border Width',
+                  type: 'range',
+                  min: 1,
+                  max: 5,
+                  step: 1,
+                  unit: 'px',
+                  property: 'border-width',
+                  selector: '.palette-playground'
+                },
+                borderColor: {
+                  type: 'color',
+                  label: 'Border Color',
+                  selector: '.palette-playground',
+                  property: 'border-color'
+                },
+                borderRadius: {
+                  label: 'Border Radius',
+                  type: 'range',
+                  min: 0,
+                  max: 50,
+                  step: 0.5,
+                  unit: '%',
+                  property: 'border-radius',
+                  selector: '.palette-playground'
+                },
+                marginTop: {
+                  label: 'Margin Top',
+                  type: 'range',
+                  min: 0,
+                  max: 20,
+                  step: 1,
+                  unit: 'px',
+                  property: 'margin-top',
+                  selector: '.palette-playground'
+                },
+                marginRight: {
+                  label: 'Margin Right',
+                  type: 'range',
+                  min: 0,
+                  max: 20,
+                  step: 1,
+                  unit: 'px',
+                  property: 'margin-right',
+                  selector: '.palette-playground'
+                },
+                marginBottom: {
+                  label: 'Margin Bottom',
+                  type: 'range',
+                  min: 0,
+                  max: 20,
+                  step: 1,
+                  unit: 'px',
+                  property: 'margin-bottom',
+                  selector: '.palette-playground'
+                },
+                marginLeft: {
+                  label: 'Margin Left',
+                  type: 'range',
+                  min: 0,
+                  max: 20,
+                  step: 1,
+                  unit: 'px',
+                  property: 'margin-left',
+                  selector: '.palette-playground'
+                },
+                paddingTop: {
+                  label: 'Padding Top',
+                  type: 'range',
+                  min: 0,
+                  max: 20,
+                  step: 1,
+                  unit: 'px',
+                  property: 'padding-top',
+                  selector: '.palette-playground'
+                },
+                paddingRight: {
+                  label: 'Padding Right',
+                  type: 'range',
+                  min: 0,
+                  max: 20,
+                  step: 1,
+                  unit: 'px',
+                  property: 'padding-right',
+                  selector: '.palette-playground'
+                },
+                paddingBottom: {
+                  label: 'Padding Bottom',
+                  type: 'range',
+                  min: 0,
+                  max: 20,
+                  step: 1,
+                  unit: 'px',
+                  property: 'padding-bottom',
+                  selector: '.palette-playground'
+                },
+                paddingLeft: {
+                  label: 'Padding Left',
+                  type: 'range',
+                  min: 0,
+                  max: 20,
+                  step: 1,
+                  unit: 'px',
+                  property: 'padding-left',
+                  selector: '.palette-playground'
+                },
+                shadow: {
+                  label: 'Shadow',
+                  type: 'color',
+                  selector: '.palette-playground',
+                  property: 'box-shadow',
+                  valueTemplate: '0 0 7px 2px %VALUE%'
+                },
+                rotation: {
+                  label: 'Rotation',
+                  type: 'range',
+                  min: 0,
+                  max: 360,
+                  step: 1,
+                  unit: 'deg',
+                  selector: '.palette-playground',
+                  property: 'transform',
+                  valueTemplate: 'rotate(%VALUE%)'
+                },
+                blur: {
+                  label: 'Blur',
+                  type: 'range',
+                  min: 0,
+                  max: 5,
+                  step: 0.1,
+                  unit: 'px',
+                  selector: '.palette-playground',
+                  property: 'filter',
+                  valueTemplate: 'blur(%VALUE%)'
+                },
+                float: {
+                  type: 'select',
+                  label: 'Float',
+                  help: 'Applied on screens with width less than 801px',
+                  selector: '.palette-playground',
+                  property: 'float',
+                  choices: [
+                    {
+                      label: 'None',
+                      value: 'none'
+                    },
+                    {
+                      label: 'Left',
+                      value: 'left'
+                    },
+                    {
+                      label: 'Right',
+                      value: 'right'
+                    }
+                  ],
+                  mediaQuery: 'screen and (max-width: 800px)'
+                },
+                opacity: {
+                  type: 'select',
+                  label: 'Opacity',
+                  help: 'Applied on screens with width more than 800px',
+                  selector: '.palette-playground',
+                  property: 'opacity',
+                  choices: [
+                    {
+                      label: '50%',
+                      value: '0.5'
+                    }
+                  ],
+                  mediaQuery: 'screen and (min-width: 801px)'
+                }
+              },
+              group: {
+                colors: {
+                  label: 'Colors',
+                  fields: [ 'textColor', 'colorVariable' ]
+                },
+                typography: {
+                  label: 'Typography',
+                  fields: [ 'textDecoration' ],
+                  group: {
+                    fonts: {
+                      label: 'Fonts',
+                      inline: true,
+                      fields: [ 'fontSize', 'fontFamily' ]
+                    }
+                  }
+                },
+                miscellaneous: {
+                  label: 'Miscellaneous',
+                  group: {
+                    border: {
+                      label: 'Border',
+                      fields: [ 'borderStyle', 'borderWidth', 'borderColor', 'borderRadius' ]
+                    },
+                    margin: {
+                      label: 'Margin',
+                      fields: [ 'marginTop', 'marginRight', 'marginBottom', 'marginLeft' ]
+                    },
+                    padding: {
+                      label: 'Padding',
+                      fields: [ 'paddingTop', 'paddingRight', 'paddingBottom', 'paddingLeft' ]
+                    }
+                  },
+                  fields: [ 'shadow', 'rotation', 'blur' ]
+                },
+                mediaQueries: {
+                  label: 'Media Queries',
+                  fields: [ 'float', 'opacity' ]
+                }
+              }
+            }
+          }
+        }
+      });
+    });
+
+    after(async function () {
+      return t.destroy(apos);
+    });
+
+    const doc = {
+      textColor: '#ff0000',
+      colorVariable: '#00ff00',
+      textDecoration: 'underline',
+      fontSize: 20,
+      fontFamily: 'Arial',
+      borderStyle: 'solid',
+      borderWidth: 2,
+      borderColor: '#0000ff',
+      borderRadius: 10,
+      marginTop: 5,
+      marginRight: 5,
+      marginBottom: 5,
+      marginLeft: 5,
+      paddingTop: 10,
+      paddingRight: 10,
+      paddingBottom: 10,
+      paddingLeft: 10,
+      shadow: '#333333',
+      rotation: 45,
+      blur: 2,
+      float: 'left',
+      opacity: '0.5'
+    };
+
+    it('should render legacy styles correctly (@apstrophecms/styles)', async function () {
+      assert.deepEqual(
+        apos.styles.stylesGroups,
+        {
+          colors: {
+            label: 'Colors',
+            fields: [ 'textColor', 'colorVariable' ]
+          },
+          typography: {
+            label: 'Typography',
+            fields: [ 'textDecoration' ],
+            group: {
+              fonts: {
+                label: 'Fonts',
+                inline: true,
+                fields: [ 'fontSize', 'fontFamily' ]
+              }
+            }
+          },
+          miscellaneous: {
+            label: 'Miscellaneous',
+            group: {
+              border: {
+                label: 'Border',
+                fields: [ 'borderStyle', 'borderWidth', 'borderColor', 'borderRadius' ]
+              },
+              margin: {
+                label: 'Margin',
+                fields: [ 'marginTop', 'marginRight', 'marginBottom', 'marginLeft' ]
+              },
+              padding: {
+                label: 'Padding',
+                fields: [ 'paddingTop', 'paddingRight', 'paddingBottom', 'paddingLeft' ]
+              }
+            },
+            fields: [ 'shadow', 'rotation', 'blur' ]
+          },
+          mediaQueries: {
+            label: 'Media Queries',
+            fields: [ 'float', 'opacity' ]
+          }
+        },
+        'Styles groups should not contain extra groups or fields'
+      );
+
+      const actual = apos.styles.getStylesheet(doc);
+      const { css } = actual;
+      const actualCss = {
+        hasSelector: css.startsWith('.palette-playground{'),
+        hasColor: css.includes('color: #ff0000;'),
+        hasDecoration: css.includes('text-decoration: underline;'),
+        hasFontSize: css.includes('font-size: 20px;'),
+        hasFontFamily: css.includes('font-family: Arial;'),
+        hasBorderStyle: css.includes('border-style: solid;'),
+        hasBorderWidth: css.includes('border-width: 2px;'),
+        hasBorderColor: css.includes('border-color: #0000ff;'),
+        hasBorderRadius: css.includes('border-radius: 10%;'),
+        hasMarginTop: css.includes('margin-top: 5px;'),
+        hasMarginRight: css.includes('margin-right: 5px;'),
+        hasMarginBottom: css.includes('margin-bottom: 5px;'),
+        hasMarginLeft: css.includes('margin-left: 5px;'),
+        hasPaddingTop: css.includes('padding-top: 10px;'),
+        hasPaddingRight: css.includes('padding-right: 10px;'),
+        hasPaddingBottom: css.includes('padding-bottom: 10px;'),
+        hasPaddingLeft: css.includes('padding-left: 10px;'),
+        hasShadow: css.includes('box-shadow: 0 0 7px 2px #333333;'),
+        hasRotation: css.includes('transform: rotate(45deg);'),
+        hasBlur: css.includes('filter: blur(2px);'),
+        hasFloatMediaQuery: css.includes('@media screen and (max-width: 800px){.palette-playground{float: left;}}'),
+        hasOpacityMediaQuery: css.includes('@media screen and (min-width: 801px){.palette-playground{opacity: 0.5;}}'),
+        hasColorVariable: css.includes(':root{--cool-variable-for-palette-testing: #00ff00;}')
+      };
+      assert.deepEqual(
+        actualCss,
+        {
+          hasSelector: true,
+          hasColor: true,
+          hasDecoration: true,
+          hasFontSize: true,
+          hasFontFamily: true,
+          hasBorderStyle: true,
+          hasBorderWidth: true,
+          hasBorderColor: true,
+          hasBorderRadius: true,
+          hasMarginTop: true,
+          hasMarginRight: true,
+          hasMarginBottom: true,
+          hasMarginLeft: true,
+          hasPaddingTop: true,
+          hasPaddingRight: true,
+          hasPaddingBottom: true,
+          hasPaddingLeft: true,
+          hasShadow: true,
+          hasRotation: true,
+          hasBlur: true,
+          hasFloatMediaQuery: true,
+          hasOpacityMediaQuery: true,
+          hasColorVariable: true
+        },
+        'Stylesheet should render all styles correctly'
+      );
+    });
+  });
 });
