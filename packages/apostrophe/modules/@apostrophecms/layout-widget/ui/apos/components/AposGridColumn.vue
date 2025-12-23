2fac3c2a8227f1256a90fb9bf19fac8b3382ec52
Jed <vjeudy@protonmail.com>
2025-12-23 19:16:36 +0100

    improves grid column component for content

--- packages/apostrophe/modules/@apostrophecms/layout-widget/ui/apos/components/AposGridColumn.vue
+++ packages/apostrophe/modules/@apostrophecms/layout-widget/ui/apos/components/AposGridColumn.vue
@@ -1,26 +1,28 @@
 <template>
   <component
     :is="props.tag"
-    ref="items"
+    :id="widgetId"
+    ref="columnEl"
+    :style="itemStyles"
+    :class="widgetStyles.classes"
     class="apos-layout__item"
     role="gridcell"
-    data-apos-test="aposManageLayoutItem"
+    data-apos-test="aposLayoutItem"
     :data-id="item._id"
-    :style="{
-      '--colstart': item.colstart,
-      '--colspan': item.colspan,
-      '--rowstart': item.rowstart,
-      '--rowspan': item.rowspan,
-      '--order': item.order,
-      '--justify': item.justify,
-      '--align': item.align,
-    }"
+    :data-tablet-full="props.tabletFullItems[item._id] || false"
+    :data-visible-tablet="item.tablet?.show"
+    :data-visible-mobile="item.mobile?.show"
   >
     <slot />
   </component>
 </template>
 
 <script setup>
+import {
+  useTemplateRef, computed, onMounted
+} from 'vue';
+import { useAposStyles } from 'Modules/@apostrophecms/styles/composables/AposStyles.js';
+
 const props = defineProps({
   item: {
     type: Object,
@@ -29,7 +31,40 @@ const props = defineProps({
   tag: {
     type: String,
     default: 'div'
+  },
+  tabletFullItems: {
+    type: Object,
+    required: true
   }
+});
+const columnEl = useTemplateRef('columnEl');
+const itemStyles = computed(() => {
+  return [
+    {
+      '--colstart': props.item.colstart,
+      '--colspan': props.item.colspan,
+      '--rowstart': props.item.rowstart,
+      '--rowspan': props.item.rowspan,
+      '--order': props.item.order,
+      '--justify': props.item.justify,
+      '--align': props.item.align
+    },
+    widgetStyles.inline
+  ];
+});
+const moduleOptions = apos.modules[apos.area.widgetManagers[props.item.type]];
+const {
+  widgetStyles,
+  widgetId,
+  getWidgetStyles
+} = useAposStyles();
+
+defineExpose({
+  columnEl
+});
+
+onMounted(() => {
+  getWidgetStyles(props.item, moduleOptions);
 });
 
 </script>
