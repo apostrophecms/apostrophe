3813fe6b58eb470dc448e675affd5708c859c380
Stuart Romanek <stuart@apostrophecms.com>
2026-01-14 15:25:23 -0500

    make sure classes are stripped when styles are set without specifying classes

--- packages/apostrophe/modules/@apostrophecms/rich-text-widget/ui/apos/components/AposRichTextWidgetEditor.vue
+++ packages/apostrophe/modules/@apostrophecms/rich-text-widget/ui/apos/components/AposRichTextWidgetEditor.vue
@@ -648,6 +648,8 @@ export default {
         // Handle custom attributes
         if (style.class) {
           style.options.class = style.class;
+        } else {
+          style.options.class = null;
         }
 
         if (!style.type) {
