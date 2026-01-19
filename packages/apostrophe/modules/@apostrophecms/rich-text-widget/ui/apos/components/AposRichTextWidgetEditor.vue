b87516ccadf1533fa220838d166659f679ed676f
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2026-01-19 16:59:22 +0200

    Handle default schema values (styles)

--- packages/apostrophe/modules/@apostrophecms/rich-text-widget/ui/apos/components/AposRichTextWidgetEditor.vue
+++ packages/apostrophe/modules/@apostrophecms/rich-text-widget/ui/apos/components/AposRichTextWidgetEditor.vue
@@ -150,6 +150,8 @@ import TableHeader from '@tiptap/extension-table-header';
 import TableRow from '@tiptap/extension-table-row';
 import Placeholder from '@tiptap/extension-placeholder';
 import { klona } from 'klona';
+import newInstance from 'apostrophe/modules/@apostrophecms/schema/lib/newInstance.js';
+import merge from 'lodash/merge';
 import { useAposStyles } from 'Modules/@apostrophecms/styles/composables/AposStyles.js';
 
 export default {
@@ -370,6 +372,12 @@ export default {
     }
   },
   mounted() {
+    const widgetInstance = newInstance(
+      this.moduleOptions.schema
+    );
+    merge(widgetInstance, this.docFields.data);
+    this.docFields.data = widgetInstance;
+
     this.getWidgetStyles(this.docFields.data, this.moduleOptions);
     this.instantiateEditor();
     apos.bus.$on('apos-refreshing', this.onAposRefreshing);
