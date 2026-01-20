4a2dcd13274050406f40dd359675755cf35f413a
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2026-01-20 13:13:49 +0200

    Validate direction schema, revert range, add box

--- packages/apostrophe/modules/@apostrophecms/schema/lib/addFieldTypes.js
+++ packages/apostrophe/modules/@apostrophecms/schema/lib/addFieldTypes.js
@@ -147,6 +147,10 @@ module.exports = (self) => {
       return !value.length;
     },
     validate(field, options, warn, fail) {
+      if (field.direction && !_.includes([ 'ltr', 'rtl' ], field.direction)) {
+        fail('The direction property must be "ltr" or "rtl" if specified');
+      }
+
       if (!field.pattern) {
         return;
       }
@@ -567,6 +571,11 @@ module.exports = (self) => {
         }
       }
       destination[field.name] = data[field.name];
+    },
+    validate(field, options, warn, fail) {
+      if (field.direction && !_.includes([ 'ltr', 'rtl' ], field.direction)) {
+        fail('The direction property must be "ltr" or "rtl" if specified');
+      }
     }
   });
 
@@ -600,6 +609,10 @@ module.exports = (self) => {
       return '';
     },
     validate(field, options, warn, fail) {
+      if (field.direction && !_.includes([ 'ltr', 'rtl' ], field.direction)) {
+        fail('The direction property must be "ltr" or "rtl" if specified');
+      }
+
       if (!field.pattern) {
         return;
       }
@@ -659,6 +672,9 @@ module.exports = (self) => {
       destination[field.name] = self.apos.launder.date(newDateVal);
     },
     validate: function (field, options, warn, fail) {
+      if (field.direction && !_.includes([ 'ltr', 'rtl' ], field.direction)) {
+        fail('The direction property must be "ltr" or "rtl" if specified');
+      }
       if (field.max && !field.max.match(dateRegex)) {
         fail('Property "max" must be in the date format, YYYY-MM-DD');
       }
@@ -723,6 +739,11 @@ module.exports = (self) => {
       destination[field.name] = data[field.name]
         ? self.apos.launder.date(data[field.name])
         : null;
+    },
+    validate: function (field, options, warn, fail) {
+      if (field.direction && !_.includes([ 'ltr', 'rtl' ], field.direction)) {
+        fail('The direction property must be "ltr" or "rtl" if specified');
+      }
     }
   });
 
@@ -739,6 +760,11 @@ module.exports = (self) => {
           field.min,
           field.max
         );
+      }
+    },
+    validate: function (field, options, warn, fail) {
+      if (field.direction && !_.includes([ 'ltr', 'rtl' ], field.direction)) {
+        fail('The direction property must be "ltr" or "rtl" if specified');
       }
     },
     def: ''
@@ -775,26 +801,6 @@ module.exports = (self) => {
         data[field.name] > field.max
       ) {
         destination[field.name] = null;
-      }
-    },
-    validate: function (field, options, warn, fail) {
-      if (!field.min && field.min !== 0) {
-        fail('Property "min" must be set.');
-      }
-      if (!field.max && field.max !== 0) {
-        fail('Property "max" must be set.');
-      }
-      if (typeof field.max !== 'number') {
-        fail('Property "max" must be a number');
-      }
-      if (typeof field.min !== 'number') {
-        fail('Property "min" must be a number');
-      }
-      if (field.step && typeof field.step !== 'number') {
-        fail('Property "step" must be a number.');
-      }
-      if (field.unit && typeof field.unit !== 'string') {
-        fail('Property "unit" must be a string.');
       }
     }
   });
