eb5675e1f589105b5268890945c55c967d39329d
Bob Means <robert.means1969@gmail.com>
2026-06-04 05:38:23 -0400

    Helpers cleanup

--- packages/apostrophe-astro/helpers/server/index.js
+++ packages/apostrophe-astro/helpers/server/index.js
@@ -11,3 +11,4 @@
 
 export { aposFetch, aposPageFetch } from './fetch.js';
 export { getAposHost, isStaticBuild } from './url.js';
+export { getAllStaticPaths, getAllUrlMetadata, getLocales } from './static.js';
