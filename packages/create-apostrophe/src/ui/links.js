60353cc4d11dc67e082a48c0eef6104db707d758
Bob Means <robert.means1969@gmail.com>
2026-06-10 09:04:36 -0400

    correct `guides` -> `guide`

--- packages/create-apostrophe/src/ui/links.js
+++ packages/create-apostrophe/src/ui/links.js
@@ -14,12 +14,12 @@ const LINKS = Object.freeze({
 
 /** Per-kit "get oriented" guides. `*-demo` and `*-demo-data` share a guide. */
 const KIT_GUIDES = Object.freeze({
-  'apostrophe-astro-essentials': 'https://apostrophecms.com/docs/guides/astro-essentials-overview.html',
-  'apostrophe-astro-demo': 'https://apostrophecms.com/docs/guides/astro-demo-overview.html',
-  'apostrophe-astro-demo-data': 'https://apostrophecms.com/docs/guides/astro-demo-overview.html',
-  'apostrophe-essentials': 'https://apostrophecms.com/docs/guides/apostrophe-standalone-essentials-overview.html',
+  'apostrophe-astro-essentials': 'https://apostrophecms.com/docs/guide/astro-essentials-overview.html',
+  'apostrophe-astro-demo': 'https://apostrophecms.com/docs/guide/astro-demo-overview.html',
+  'apostrophe-astro-demo-data': 'https://apostrophecms.com/docs/guide/astro-demo-overview.html',
+  'apostrophe-essentials': 'https://apostrophecms.com/docs/guide/apostrophe-standalone-essentials-overview.html',
   'apostrophe-demo': 'https://apostrophecms.com/docs/guides/apostrophe-demo-overview.html',
-  'apostrophe-demo-data': 'https://apostrophecms.com/docs/guides/apostrophe-demo-overview.html'
+  'apostrophe-demo-data': 'https://apostrophecms.com/docs/guide/apostrophe-demo-overview.html'
 });
 
 /** @typedef {keyof typeof LINKS} LinkName */
