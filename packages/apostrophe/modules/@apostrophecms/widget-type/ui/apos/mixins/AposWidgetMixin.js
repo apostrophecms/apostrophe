761c18f7aa83b3bc41cd7fbbe04529bcaf4e010b
Thomas Boutell <tom@apostrophecms.com>
2026-07-26 10:56:23 -0400

    self-debouncing widget renders. Additional work could be done to get rid of the nonfunctional async plumbing elsewhere that never really worked

--- packages/apostrophe/modules/@apostrophecms/widget-type/ui/apos/mixins/AposWidgetMixin.js
+++ packages/apostrophe/modules/@apostrophecms/widget-type/ui/apos/mixins/AposWidgetMixin.js
@@ -1,3 +1,4 @@
+import { klona } from 'klona';
 import props from '../composables/AposWidgetProps.js';
 import {
   _renderContent, _emitWidgetRendered, _getClasses
@@ -7,7 +8,10 @@ export default {
   props,
   data() {
     return {
-      rendered: '...'
+      rendered: '...',
+      renderId: 0,
+      lastShownRenderId: -1,
+      active: false
     };
   },
   watch: {
@@ -31,16 +35,45 @@ export default {
     getClasses() {
       return _getClasses(this.modelValue, this.moduleOptions);
     },
+    // This method id fire-and-forget. It uses async internally to
+    // debounce simultaneous async requests
     async renderContent() {
-      const result = await _renderContent(this.$props);
-      if (Object.hasOwn(result, 'data')) {
-        this.rendered = result.data;
+      this.renderId++;
+      // Because it can change in a later invocation during
+      // the network request
+      const renderId = this.renderId;
+      if (this.active) {
+        // Wait for the current render request to finish
+        await this.active;
+        console.log('after the wait');
+        if (renderId < this.renderId) {
+          // Skip this render request, a newer one has shown up
+          // in the meantime
+          return;
+        }
       }
-      if (!result.error) {
-        this.$nextTick(() => {
-          _emitWidgetRendered(this.modelValue.aposLivePreview, { el: this.$el });
-        });
-      }
+      // We didn't get skipped and it's our turn now
+      this.active = (async () => {
+        console.log('starting');
+        const result = await _renderContent(this.$props);
+        this.active = false;
+        if (renderId < this.lastShownRenderId) {
+          // The network can return renders out of order,
+          // never display something more stale than the
+          // last thing displayed
+          return;
+        }
+        this.lastShownRenderId = renderId;
+        if (Object.hasOwn(result, 'data')) {
+          this.rendered = result.data;
+        }
+        if (!result.error) {
+          this.$nextTick(() => {
+            _emitWidgetRendered(this.modelValue.aposLivePreview, { el: this.$el });
+          });
+        }
+        console.log('finished');
+      })();
     }
   }
 };
