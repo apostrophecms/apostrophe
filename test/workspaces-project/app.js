6aff3c94e31c7ff69af51ec573ed2445ec3063a3
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2026-08-04 14:28:44 +0300

    Fix outdated fixture

--- test/workspaces-project/app.js
+++ test/workspaces-project/app.js
@@ -1,16 +1,13 @@
 module.exports = {
   root: module,
   shortName: 'workspaces-project',
+  baseUrl: 'http://localhost:3000',
   modules: {
     '@apostrophecms/express': {
       options: {
         address: '127.0.0.1'
       }
     },
-    '@apostrophecms/sitemap': {
-      options: {
-        baseUrl: 'http://localhost:3000'
-      }
-    }
+    '@apostrophecms/sitemap': {}
   }
 };
